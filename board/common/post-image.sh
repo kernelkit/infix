06c1306dc5dc907c3d371bf85da8ced5a8ce0f1b
Joachim Wiberg <troglobit@gmail.com>
2022-05-17 08:49:31 +0200

    board/common: enable kernel quiet mode by default
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- board/common/post-image.sh
+++ board/common/post-image.sh
@@ -7,9 +7,9 @@ cat <<-EOF >"$BINARIES_DIR/qemu.sh"
 	#!/bin/sh
 	line=\$(stty -g)
 	stty raw
-	qemu-system-x86_64 -M pc -cpu kvm64 -enable-kvm -nographic		\\
-	    -kernel bzImage -append "rootwait root=/dev/vda console=ttyS0"	\\
-	    -drive file=rootfs.ext2,if=virtio,format=raw			\\
+	qemu-system-x86_64 -M pc -cpu kvm64 -enable-kvm -nographic			\\
+	    -kernel bzImage -append "rootwait root=/dev/vda console=ttyS0 quiet"	\\
+	    -drive file=rootfs.ext2,if=virtio,format=raw				\\
 	    -net nic,model=virtio -net user
 	stty "\$line"
 EOF
