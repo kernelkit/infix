38b5c16dc1dec590981dd680fb2be056e67a0d22
Joachim Wiberg <troglobit@gmail.com>
2022-05-18 08:33:23 +0200

    board/common: generate /etc/os-release in new post-build.sh
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

---
+++ board/common/post-build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# This is a symlink to /usr/lib/os-release, so we remove this to keep
+# original Buildroot information.
+rm "$TARGET_DIR/etc/os-release"
+{
+	echo "NAME=\"Inf/IX\""
+	echo "VERSION=${BR2_VERSION}"
+	echo "ID=infix"
+	echo "VERSION_ID=${BR2_VERSION}"
+	echo "PRETTY_NAME=\"Inf/IX by KernelKit\""
+	echo "VARIANT=AMD64"
+	echo "VARIANT_ID=amd64"
+	echo "HOME_URL=https://github.com/KernelKit"
+} > "$TARGET_DIR/etc/os-release"
+
+echo "Inf/IX by KernelKit $BR2_VERSION -- $(date +"%b %e %H:%M %Z %Y")" > "$TARGET_DIR/etc/version"
