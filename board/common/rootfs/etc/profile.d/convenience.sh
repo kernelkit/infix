c4e6a24a478028bbd7b8d85e15f36f68ccea8c39
Tobias Waldekranz <tobias@waldekranz.com>
2022-10-06 10:42:53 +0200

    rootfs: Add common overlay
    Start out with some convenience macros and a proper PS1.

---
+++ board/common/rootfs/etc/profile.d/convenience.sh
@@ -0,0 +1,18 @@
+alias l='ls -CF'
+alias la='ls -A'
+alias ll='ls -alF'
+alias ls='ls --color=auto'
+
+alias vim='vi'
+alias view='vi -R'
+alias emacs='mg'
+alias edit='mg'
+alias editor=edit
+alias sensible-editor=edit
+alias sensible-pager=less
+export EDITOR=/usr/bin/mg
+
+alias ip='ip --color=auto'
+alias ipb='ip -br'
+alias bridge='bridge --color=auto'
+
