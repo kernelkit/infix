a3f2e2a3005b278fb56b62224f0e41601048721a
Joachim Wiberg <troglobit@gmail.com>
2022-11-18 10:20:10 +0100

    aarch64: replace package override with local.mk rule in external.mk
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- external.mk
+++ external.mk
@@ -1,5 +1,10 @@
 include $(sort $(wildcard $(BR2_EXTERNAL_INFIX_PATH)/package/*/*.mk))
 
+.PHONY:
+local.mk:
+	@echo "Installing local override for certain packages ..."
+	@(cd $O && ln -s $(BR2_EXTERNAL_INFIX_PATH)/local.mk .)
+
 .PHONY: run
 run:
 	@echo "Starting Qemu  ::  Ctrl-a x -- exit | Ctrl-a c -- toggle console/monitor"
