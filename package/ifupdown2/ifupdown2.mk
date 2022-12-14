e3c772e25a6da6100840b36a589e89369535402c
Joachim Wiberg <troglobit@gmail.com>
2022-12-14 23:05:07 +0100

    package/ifupdown2: bump to latest git hash
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- package/ifupdown2/ifupdown2.mk
+++ package/ifupdown2/ifupdown2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-IFUPDOWN2_VERSION = a97d3e0cf96ab44e1e37a5940c83ea390f6656c7
+IFUPDOWN2_VERSION = db0f7548189dde918bfc37dd864cf935d5384a83
 IFUPDOWN2_SITE = $(call github,kernelkit,ifupdown2,$(IFUPDOWN2_VERSION))
 IFUPDOWN2_LICENSE = GPL-2.0+
 IFUPDOWN2_LICENSE_FILES = LICENSE
