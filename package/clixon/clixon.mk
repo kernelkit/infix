41737b2343f4b24d07403fccf7652c87c90cd3c8
Joachim Wiberg <troglobit@gmail.com>
2023-02-06 11:23:20 +0100

    package/clixon: new package
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

---
+++ package/clixon/clixon.mk
@@ -0,0 +1,60 @@
+################################################################################
+#
+# clixon
+#
+################################################################################
+
+CLIXON_VERSION = 6.0.0
+CLIXON_SITE = $(call github,clicon,clixon,$(CLIXON_VERSION))
+CLIXON_LICENSE = Apache-2.0
+CLIXON_LICENSE_FILES = LICENSE.md
+CLIXON_INSTALL_STAGING = YES
+CLIXON_DEPENDENCIES = cligen #libcurl libxml2
+
+ifeq ($(BR_PACKAGE_CLIXON_RESTCONF_NATIVE),y)
+CLIXON_CONF_OPTS += --with-restconf=native
+endif
+
+ifeq ($(BR_PACKAGE_CLIXON_HTTP1),y)
+CLIXON_CONF_OPTS += --enable-http1
+else
+CLIXON_CONF_OPTS += --disable-http1
+endif
+
+ifeq ($(BR_PACKAGE_CLIXON_HTTP2),y)
+CLIXON_DEPENDENCIES += nghttp2
+CLIXON_CONF_OPTS += --enable-nghttp2
+else
+CLIXON_CONF_OPTS += --disable-nghttp2
+endif
+
+ifeq ($(BR_PACKAGE_CLIXON_RESTCONF_FCGI),y)
+CLIXON_DEPENDENCIES += nginx
+CLIXON_CONF_OPTS += --with-restconf=fcgi
+endif
+
+ifeq ($(BR_PACKAGE_CLIXON_RESTCONF_NONE),y)
+CLIXON_CONF_OPTS += --without-restconf
+endif
+
+ifeq ($(BR2_PACKAGE_CLIXON_PUBLISH),y)
+CLIXON_CONF_OPTS += --enable-publish
+else
+CLIXON_CONF_OPTS += --disable-publish
+endif
+
+ifeq ($(BR2_PACKAGE_CLIXON_SNMP),y)
+CLIXON_DEPENDENCIES += netsnmp
+CLIXON_CONF_OPTS += --enable-netsnmp
+else
+CLIXON_CONF_OPTS += --disable-netsnmp
+endif
+
+ifeq ($(BR2_PACKAGE_CLIXON_EXAMPLE),y)
+define CLIXON_INSTALL_EXAMPLE
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) DESTDIR=$(TARGET_DIR) install-example
+endef
+CLIXON_POST_INSTALL_TARGET_HOOKS += CLIXON_INSTALL_EXAMPLE
+endif
+
+$(eval $(autotools-package))
