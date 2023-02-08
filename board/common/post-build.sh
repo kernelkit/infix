16af6f9622f8e657e98314beeda2747009305b64
Tobias Waldekranz <tobias@waldekranz.com>
2023-02-08 19:46:46 +0100

    board/common: Remove remnants of dropbear key generation

--- board/common/post-build.sh
+++ board/common/post-build.sh
@@ -16,12 +16,6 @@ rm "$TARGET_DIR/etc/os-release"
 
 echo "Infix by KernelKit $GIT_VERSION -- $(date +"%b %e %H:%M %Z %Y")" > "$TARGET_DIR/etc/version"
 
-# Prevent regen of host key at every boot, /etc is saved across reboots
-if [ -L "$TARGET_DIR/etc/dropbear" ]; then
-	rm    "$TARGET_DIR/etc/dropbear"
-	mkdir "$TARGET_DIR/etc/dropbear"
-fi
-
 if [ -f "$TARGET_DIR/etc/dhcpcd.conf" ]; then
 	if grep -vq background  "$TARGET_DIR/etc/dhcpcd.conf"; then
 		cat <<-EOF >> "$TARGET_DIR/etc/dhcpcd.conf"
