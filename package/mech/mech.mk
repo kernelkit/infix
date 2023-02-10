3d351943414be17737aef69de97b01badc78d597
Tobias Waldekranz <tobias@waldekranz.com>
2023-02-10 16:04:43 +0100

    mech: Add infrastructure for backend plugins using Augeas

--- package/mech/mech.mk
+++ package/mech/mech.mk
@@ -7,6 +7,7 @@ MECH_VERSION = 1.0
 MECH_LICENSE = GPL
 MECH_SITE_METHOD = local
 MECH_SITE = $(BR2_EXTERNAL_INFIX_PATH)/src/mech
+MECH_DEPENDENCIES = augeas clixon
 MECH_AUTORECONF = YES
 
 $(eval $(autotools-package))
