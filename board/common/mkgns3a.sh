8e7b173de3d81fc3a8856cd6e5094cfd241eb109
Joachim Wiberg <troglobit@gmail.com>
2023-02-22 07:32:43 +0100

    mkgns3a.sh: increase node ram, we now provide Frr
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- board/common/mkgns3a.sh
+++ board/common/mkgns3a.sh
@@ -18,7 +18,7 @@ cat <<EOF >"$BINARIES_DIR/infix.gns3a"
     "qemu": {
         "adapter_type": "virtio-net-pci",
         "adapters": 10,
-        "ram": 256,
+        "ram": 384,
         "cpus": 1,
         "hda_disk_interface": "virtio",
         "arch": "x86_64",
