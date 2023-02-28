bc7b366c858b52cbd9fd6bf3201fd779a9c0d0f7
Joachim Wiberg <troglobit@gmail.com>
2023-02-28 11:34:12 +0100

    package/cligen: prevent host install from stripping .so files
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- package/cligen/cligen.mk
+++ package/cligen/cligen.mk
@@ -9,5 +9,6 @@ CLIGEN_SITE = $(call github,clicon,cligen,$(CLIGEN_VERSION))
 CLIGEN_LICENSE = Apache-2.0
 CLIGEN_LICENSE_FILES = LICENSE.md
 CLIGEN_INSTALL_STAGING = YES
+CLIGEN_CONF_OPTS = INSTALLFLAGS=" "
 
 $(eval $(autotools-package))
