046b15fbc042dec4b94d89ef6abac37972d31d0f
Joachim Wiberg <troglobit@gmail.com>
2023-02-28 11:33:35 +0100

    common: separate handling of aarch64 and amd64 images, for now
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- board/common/post-image.sh
+++ board/common/post-image.sh
@@ -4,14 +4,18 @@
 
 common=$(dirname "$(readlink -f "$0")")
 
-imgdir=$1
-arch=$2
-signkey=$3
+# Temporary, separate handling of aarch64 and amd64 images.
+# Best would be to have the same for both, i.e., boot GNS3
+# with u-boot.
+if [ "$BR2_ARCH" = "aarch64" ]; then
+    # shellcheck disable=SC2034
+    imgdir=$1
+    arch=$2
+    signkey=$3
 
-$common/sign.sh $arch $signkey
-$common/mkfit.sh
-$common/mkmmc.sh
-
-if [ "$BR2_ARCH" = "x86_64" ]; then
-	"$common/mkgns3a.sh"
+    $common/sign.sh $arch $signkey
+    $common/mkfit.sh
+    $common/mkmmc.sh
+elif [ "$BR2_ARCH" = "x86_64" ]; then
+    "$common/mkgns3a.sh"
 fi
