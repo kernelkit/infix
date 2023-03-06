221cc0b1f68195db250dc1337dab22bde5bfe1b4
Joachim Wiberg <troglobit@gmail.com>
2023-03-06 09:53:47 +0100

    mech: move Finit service .conf to package and add conditions
    The new if: conditions require latest Finit master, unreleased v4.4
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- package/mech/mech.mk
+++ package/mech/mech.mk
@@ -10,4 +10,10 @@ MECH_SITE = $(BR2_EXTERNAL_INFIX_PATH)/src/mech
 MECH_DEPENDENCIES = augeas clixon
 MECH_AUTORECONF = YES
 
+define MECH_INSTALL_EXTRA
+	cp $(MECH_PKGDIR)/clixon.conf   $(FINIT_D)/available/
+	ln -sf ../available/clixon.conf $(FINIT_D)/enabled/clixon.conf
+endef
+MECH_TARGET_FINALIZE_HOOKS += MECH_INSTALL_EXTRA
+
 $(eval $(autotools-package))
