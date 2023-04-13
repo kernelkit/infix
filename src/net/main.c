5ce2e57515ab3342922a78e0ec333f9a33384732
Joachim Wiberg <troglobit@gmail.com>
2023-04-13 09:45:09 +0200

    src/net: calculate and run teardown in reverse dependency order
    When taking down a network setup, it needs to be deconstructed using the
    foo.exit rules of the previous generation, and in reverse dependency
    order.
    
    This patch adds support for generating the deps when bringing up the
    network setup, and saving an rdeps file in that generation, which can
    be used later when moving leafs between nodes.
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- src/net/main.c
+++ src/net/main.c
@@ -6,11 +6,17 @@
 #include <libite/lite.h>
 
 #define _PATH_NET "/run/net"
-#define DEBUG 0
-#define dbg(fmt, args...) if (DEBUG) warnx(fmt, ##args)
+#define dbg(fmt, args...) if (debug)   warnx(fmt, ##args)
+#define log(fmt, args...) if (verbose) warnx(fmt, ##args)
 
 static char **handled;
 static int if_num;
+static int debug;
+static int verbose;
+static int dep;
+
+static FILE *ip, *bridge;
+extern char *__progname;
 
 
 static void if_alloc(int num)
@@ -52,6 +58,89 @@ static int if_find(char *ifname)
 	return 0;
 }
 
+static void savedep(char *ipath)
+{
+	char *ifname = basename(ipath);
+	char *path = dirname(ipath);
+	char buf[20];
+	FILE *fp;
+
+	if (!strcmp(ifname, "deps"))
+		return;
+
+	fp = fopenf("a+", "%s/deps", path);
+	if (!fp)
+		return;
+
+	(void)fseek(fp, 0L, SEEK_SET);
+	while (fgets(buf, sizeof(buf), fp)) {
+		if (!strcmp(chomp(buf), ifname))
+			goto done;
+	}
+
+	dep++;
+	fprintf(fp, "%s\n", ifname);
+done:
+	fclose(fp);
+}
+
+static int save_rdeps(const char *path, char *gen)
+{
+	char ipath[strlen(path) + strlen(gen) + 16 + 4];
+#if 0
+	char **list;
+	int i, num;
+
+	snprintf(ipath, sizeof(ipath), "%s/%s", path, gen);
+	num = dir(ipath, NULL, NULL, &list, 0);
+	if (!num)
+		return -1;
+
+	for (i = 0; i < num; i++) {
+		snprintf(ipath, sizeof(ipath), "%s/%s/%s", path, gen, list[i]);
+		if (access(ipath, X_OK))
+			continue;
+
+		savedep(ipath);
+	}
+#endif
+	snprintf(ipath, sizeof(ipath), "%s/%s", path, gen);
+	return systemf("sed '1!G;h;$!d' < %s/deps >%s/rdeps", ipath, ipath);
+}
+
+static int pipeit(FILE *pp, const char *action)
+{
+	char line[256];
+	FILE *fp;
+
+	fp = fopen(action, "r");
+	if (!fp)
+		return 0;	/* nop */
+
+	log("running %s ...", action);
+
+	while (fgets(line, sizeof(line), fp))
+		fputs(line, pp);
+
+	return fclose(fp);
+}
+
+static int run(const char *action)
+{
+	int rc;
+
+	if (strstr(action, "/ip.") || strstr(action, "/lag."))
+		return pipeit(ip, action);
+	if (strstr(action, "/bridge."))
+		return pipeit(bridge, action);
+
+	 /* ethool does not support batch mode (yet) */
+	rc = systemf("%s", action);
+	if (rc)
+		warn("failed %s, rc %d", action, rc);
+	return 0;
+}
+
 static int deps(char *ipath, char *ifname, const char *action)
 {
 	char path[strlen(ipath) + 42];
@@ -76,18 +165,19 @@ static int deps(char *ipath, char *ifname, const char *action)
 		free(rp);
 	}
 
+	savedep(ipath);
 	if (if_find(ifname))
 		return 0;
 
 	snprintf(path, sizeof(path), "%s/%s", ipath, action);
 	cmd = realpath(path, NULL);
-	if (!cmd || access(cmd, X_OK)) {
-		if (errno == ENOENT)
+	if (!cmd || access(cmd, F_OK)) {
+		if (errno == ENOENT || errno == ENOTDIR)
 			rc = 0;	/* no action for this interface */
 		goto done;
 	}
 
-	rc = systemf("%s", cmd);
+	rc = run(cmd);
 done:
 	free(cmd);
 	if_done(ifname);
@@ -111,6 +201,7 @@ static int iter(char *path, size_t len, const char *action)
 		snprintf(ipath, sizeof(ipath), "%s/%s", path, ifname);
 		dbg("Calling deps(%s, %s, %s)", ipath, ifname, action);
 		rc += deps(ipath, ifname, action);
+		dbg("rc => %d", rc);
 		free(ifname);
 	}
 
@@ -119,19 +210,42 @@ static int iter(char *path, size_t len, const char *action)
 	return rc;
 }
 
+static int rdeps(char *path, size_t len, const char *action)
+{
+	char cmd[len + 20 + strlen(action)];
+	char ifname[20];
+	int rc = 0;
+	FILE *fp;
+
+	fp = fopenf("r", "%s/rdeps", path);
+	if (!fp)
+		return 0;	/* no deps in prev. generation */
+
+	while (fgets(ifname, sizeof(ifname), fp)) {
+		snprintf(cmd, sizeof(cmd), "%s/%s/%s", path, chomp(ifname), action);
+		if (access(cmd, F_OK)) {
+			dbg("skipping %s, errno %d: %s", cmd, errno, strerror(errno));
+			continue;
+		}
+		rc += run(cmd);
+	}
+
+	return rc;
+}
+
 static int deactivate(const char *net, char *gen)
 {
 	char path[strlen(net) + strlen(gen) + 5 + IFNAMSIZ];
 	char *action[] = {
-		"ip.exit",
 		"bridge.exit",
+		"ip.exit",
 		"ethtool.exit",
 	};
 	int rc = 0;
 
 	snprintf(path, sizeof(path), "%s/%s", net, gen);
 	for (size_t i = 0; i < NELEMS(action); i++)
-		rc += iter(path, sizeof(path), action[i]);
+		rc += rdeps(path, sizeof(path), action[i]);
 
 	return rc;
 }
@@ -141,8 +255,8 @@ static int activate(const char *net, char *gen)
 	char path[strlen(net) + strlen(gen) + 5 + IFNAMSIZ];
 	char *action[] = {
 		"ethtool.init",
-		"bridge.init",
 		"ip.init",
+		"bridge.init",
 	};
 	int rc = 0;
 
@@ -181,11 +295,50 @@ static int save_gen(const char *net, char *gen, char *buf)
 	return 0;
 }
 
-int main(void)
+static void pipe_init(void)
+{
+	ip = popen("ip -batch -", "w");
+	if (!ip)
+		err(1, "failed starting ip command pipe");
+
+	bridge = popen("bridge -batch -", "w");
+	if (!bridge)
+		err(1, "failed starting bridge command pipe");
+}
+
+static int usage(int code)
+{
+	printf("Usage: %s [-dh]\n"
+	       "\n"
+	       "Options:\n"
+	       "  -d      Debug\n"
+	       "  -h      This help text\n"
+	       "  -v      Verbose, show actions taken\n"
+	       "\n", __progname);
+
+	return code;
+}
+
+int main(int argc, char *argv[])
 {
 	const char *net = _PATH_NET;
 	char curr[512], next[512];
-	int rc;
+	int rc, c;
+
+	while ((c = getopt(argc, argv, "dhv")) != EOF) {
+		switch (c) {
+		case 'd':
+			debug = 1;
+			break;
+		case 'h':
+			return usage(0);
+		case 'v':
+			verbose = 1;
+			break;
+		default:
+			return usage(1);
+		}
+	}
 
 	if (getenv("NET_DIR"))
 		net = getenv("NET_DIR");
@@ -193,6 +346,8 @@ int main(void)
 		if (makedir(net, 0755))
 			err(1, "makedir");
 	}
+
+	pipe_init();
 
 	if ((rc = load_gen(net, "next", next, sizeof(next)))) {
 		if (rc == EX_IOERR)
@@ -213,6 +368,9 @@ int main(void)
 	rc = activate(net, next);
 	if (rc)
 		err(1, "failed activating next generation");
+
+	if (save_rdeps(net, next))
+		err(1, "failed saving interface deps in %s", next);
 
 	if (save_gen(net, "gen", next))
 		err(1, "next generation applied, failed current");
