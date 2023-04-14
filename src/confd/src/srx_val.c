9af0bf398a62f4d81ad99705b464649004ec1a0f
Joachim Wiberg <troglobit@gmail.com>
2023-04-14 16:25:46 +0200

    src/confd: refactor sr_get_bool() into srx_get_bool()
    Note: new API, srx_get_bool() returns -1 on error, leaving 0 and 1 as
    false and true, respectively.
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- src/confd/src/srx_val.c
+++ src/confd/src/srx_val.c
@@ -1,37 +1,68 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 
+#include <stdarg.h>
 #include "core.h"
 
-char *srx_get_str(sr_session_ctx_t *session, const char *fmt, ...)
+static int srx_vaget(sr_session_ctx_t *session, const char *fmt, va_list ap, sr_val_t **val, sr_val_type_t type)
 {
-	char *str = NULL;
-	sr_val_t *val;
+	va_list apdup;
 	char *xpath;
-	va_list ap;
 	int len;
 
-	va_start(ap, fmt);
-	len = vsnprintf(NULL, 0, fmt, ap) + 1;
-	va_end(ap);
+	va_copy(apdup, ap);
+	len = vsnprintf(NULL, 0, fmt, apdup) + 1;
+	va_end(apdup);
 
 	xpath = alloca(len);
 	if (!xpath)
-		return NULL;
+		return -1;
+
+	va_copy(apdup, ap);
+	vsnprintf(xpath, len, fmt, apdup);
+	va_end(apdup);
+
+	if (sr_get_item(session, xpath, 0, val)) {
+		ERROR("Failed reading xpath %s", xpath);
+		return -1;
+	}
+
+	if (type != SR_UNKNOWN_T && (*val)->type != type)
+		return -1;
+
+	return 0;
+}
+
+int srx_get_bool(sr_session_ctx_t *session, const char *fmt, ...)
+{
+	sr_val_t *val = NULL;
+	int result = -1;
+	va_list ap;
 
 	va_start(ap, fmt);
-	vsnprintf(xpath, len, fmt, ap);
-	va_end(ap);
-
-	if (sr_get_item(session, xpath, 0, &val)) {
-		ERROR("Failed reading string value from xpath %s", xpath);
-		goto fail;
-	}
-	if (!val || val->type != SR_STRING_T)
+	if (srx_vaget(session, fmt, ap, &val, SR_BOOL_T))
 		goto fail;
 
-	str = strdup(val->data.string_val);
+	result = val->data.bool_val;
+	sr_free_val(val);
 fail:
-	if (val)
-		sr_free_val(val);
+	va_end(ap);
+	return result;
+}
+
+
+char *srx_get_str(sr_session_ctx_t *session, const char *fmt, ...)
+{
+	sr_val_t *val = NULL;
+	char *str = NULL;
+	va_list ap;
+
+	va_start(ap, fmt);
+	if (srx_vaget(session, fmt, ap, &val, SR_STRING_T))
+		goto fail;
+
+	str = sr_val_to_str(val);
+	sr_free_val(val);
+fail:
+	va_end(ap);
 	return str;
 }
