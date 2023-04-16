130991aaf1b1641524ca7edf78b6c2e0ad7b8af8
Joachim Wiberg <troglobit@gmail.com>
2023-04-16 12:22:37 +0200

    src/confd: add support for ietf-ip:ipv4/forwarding
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- src/confd/src/helpers.c
+++ src/confd/src/helpers.c
@@ -4,6 +4,26 @@
 #include "core.h"
 
 /*
+ * Write interger value to a file composed from fmt and optional args.
+ */
+int writedf(int value, const char *fmt, ...)
+{
+	va_list ap;
+	int rc = -1;
+	FILE *fp;
+
+	va_start(ap, fmt);
+	fp = fopenf("w", fmt, ap);
+	if (fp) {
+		fprintf(fp, "%d\n", value);
+		rc = fclose(fp);
+	}
+	va_end(ap);
+
+	return rc;
+}
+
+/*
  * Write str to a file composed from fmt and optional args.
  */
 int writesf(const char *str, const char *fmt, ...)
