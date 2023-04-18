34689ea374309605d3c0cb0554aa3ce5d58748ff
Joachim Wiberg <troglobit@gmail.com>
2023-04-18 19:34:20 +0200

    src/net: new test, basic bridge
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

---
+++ src/net/test/bridge.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+# - Basic bridge with three bridge ports
+# - Add another port
+# - Remove a port
+
+TEST_DIR=$(dirname "$0")
+. "$TEST_DIR/lib.sh"
+
+################################################
+say "Verify bringup of basic bridge with three ports"
+create_iface eth0
+create_iface eth1
+create_iface eth2
+create_bridge br0 eth0 eth1 eth2
+
+netdo
+
+assert_bridge_ports br0 true eth0 eth1 eth2
+assert_iface br0
+
+################################################
+sep
+say "Verify add another bridge port"
+create_ng
+
+create_iface br0
+create_iface eth3
+add_brport br0 eth3
+
+netdo
+
+assert_bridge_ports br0 true eth0 eth1 eth2 eth3
+assert_iface br0
+
+################################################
+sep
+say "Verify delete a bridge port"
+del_brport br0 eth1
+create_ng
+
+create_iface br0
+create_iface eth1
+
+netdo
+
+bridge link
+assert_bridge_ports br0 true eth0 eth2 eth3
+assert_bridge_ports br0 false eth1
+assert_iface br0
