7dde00c6b034823a5d3e46fd66bde10b9ce4d875
Joachim Wiberg <troglobit@gmail.com>
2023-04-24 17:24:45 +0200

    package/confd: rename factory.cfg -> factory-config.cfg
    Consistent naming to match startup-config.cfg and running-config.cfg
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- package/confd/confd.mk
+++ package/confd/confd.mk
@@ -16,7 +16,7 @@ define CONFD_INSTALL_EXTRA
 	ln -sf ../available/sysrepo.conf $(FINIT_D)/enabled/sysrepo.conf
 	cp $(CONFD_PKGDIR)/tmpfiles.conf $(TARGET_DIR)/etc/tmpfiles.d/confd.conf
 	mkdir -p $(TARGET_DIR)/usr/share/factory/cfg
-	cp $(CONFD_PKGDIR)/factory.cfg   $(TARGET_DIR)/usr/share/factory/cfg/startup-config.cfg
+	cp $(CONFD_PKGDIR)/factory-config.cfg $(TARGET_DIR)/usr/share/factory/cfg/startup-config.cfg
 	mkdir -p $(TARGET_DIR)/lib/infix
 	cp $(CONFD_PKGDIR)/prep-db       $(TARGET_DIR)/lib/infix/
 	cp $(CONFD_PKGDIR)/clean-etc     $(TARGET_DIR)/lib/infix/
