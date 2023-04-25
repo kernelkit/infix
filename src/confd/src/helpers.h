7c3d55bcc1ca2ff340b94e821d08e393c07ec37f
Tobias Waldekranz <tobias@waldekranz.com>
2023-04-25 14:58:53 +0200

    confd: Infer type when configuring physical interfaces

--- src/confd/src/helpers.h
+++ src/confd/src/helpers.h
@@ -3,6 +3,8 @@
 #ifndef CONFD_HELPERS_H_
 #define CONFD_HELPERS_H_
 
+FILE *popenf(const char *type, const char *cmdf, ...);
+
 int writedf(int value, const char *fmt, ...);
 int writesf(const char *str, const char *fmt, ...);
 
