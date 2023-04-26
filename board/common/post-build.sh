8158d3bffbd8d60b588d8ff65913580a92030b51
Joachim Wiberg <troglobit@gmail.com>
2023-04-26 12:33:48 +0200

    Simplify and refine NETCONF and Classic build variants
     - board/common: drop everything not needed by NETCONF mode
     - board/classic: most board/common helpers relocated here
     - Simplify Finit scripts for critical services
     - Source variant from /etc/os-release to select ramdisk /etc
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- board/common/post-build.sh
+++ board/common/post-build.sh
@@ -11,16 +11,24 @@ GIT_VERSION=$(git -C $BR2_EXTERNAL_INFIX_PATH describe --always --dirty --tags)
 # original Buildroot information.
 rm "$TARGET_DIR/etc/os-release"
 {
-	echo "NAME=\"Infix\""
-	echo "VERSION=${GIT_VERSION}"
-	echo "ID=infix"
-	echo "ID_LIKE=\"${ID}\""
-	echo "VERSION_ID=${GIT_VERSION}"
-	echo "BUILD_ID=\"${NAME} ${VERSION}\""
-	echo "PRETTY_NAME=\"Infix by KernelKit\""
-	echo "ARCHITECTURE=\"${INFIX_ARCH}\""
-	echo "HOME_URL=https://github.com/KernelKit"
+    echo "NAME=\"Infix\""
+    echo "VERSION=${GIT_VERSION}"
+    echo "ID=infix"
+    echo "ID_LIKE=\"${ID}\""
+    echo "VERSION_ID=${GIT_VERSION}"
+    echo "BUILD_ID=\"${NAME} ${VERSION}\""
+    echo "PRETTY_NAME=\"Infix by KernelKit\""
+    if [ "$INFIX_VARIANT_NETCONF" = "y" ]; then
+	echo "VARIANT=\"Managed NETCONF\""
+	echo "VARIANT_ID=netconf"
+    else
+	echo "VARIANT=\"Classic, writable /etc\""
+	echo "VARIANT_ID=classic"
+    fi
+    echo "ARCHITECTURE=\"${INFIX_ARCH}\""
+    echo "HOME_URL=https://github.com/KernelKit"
 } > "$TARGET_DIR/etc/os-release"
+
 
 echo "Infix by KernelKit $GIT_VERSION -- $(date +"%b %e %H:%M %Z %Y")" > "$TARGET_DIR/etc/version"
 
