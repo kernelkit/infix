8aed30552ffdc1a17b6396fc80ffc68d27ca767c
Joachim Wiberg <troglobit@gmail.com>
2023-05-15 19:17:23 +0200

    package/finit: bump for Finit v4.4 (GA)
    Full release notes: https://github.com/troglobit/finit/releases/tag/4.4
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- package/finit/finit.mk
+++ package/finit/finit.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FINIT_VERSION = 4.3
+FINIT_VERSION = 4.4
 FINIT_SITE = https://github.com/troglobit/finit/releases/download/$(FINIT_VERSION)
 FINIT_LICENSE = MIT
 FINIT_LICENSE_FILES = LICENSE
@@ -14,10 +14,10 @@ FINIT_INSTALL_STAGING = YES
 FINIT_D = $(TARGET_DIR)/etc/finit.d
 
 # Create configure script using autoreconf when building from git
-FINIT_VERSION = 438d6b4e638418a2a22024a3cead2f47909d72b9
-FINIT_SITE = $(call github,troglobit,finit,$(FINIT_VERSION))
-FINIT_AUTORECONF = YES
-FINIT_DEPENDENCIES += host-automake host-autoconf host-libtool
+#FINIT_VERSION = 438d6b4e638418a2a22024a3cead2f47909d72b9
+#FINIT_SITE = $(call github,troglobit,finit,$(FINIT_VERSION))
+#FINIT_AUTORECONF = YES
+#FINIT_DEPENDENCIES += host-automake host-autoconf host-libtool
 
 # Buildroot defaults to /usr for both prefix and exec-prefix, this we
 # must override because we want to install into /sbin and /bin for the
