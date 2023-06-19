04998a2d66c9b901c53c4216c108f3c6e2d6c5df
Richard Alpe <richard@bit42.se>
2023-06-19 14:24:14 +0200

    statd: break out operational status into new daemon
    This patch does two main things:
    1) Breaks "statd" free from "confd".
    2) Introduces netlink monitoring of interfaces for sysrepo data.
    
    Prior to this patch, sysrepo operation status was handled by code
    named "statd" inside "confd". (Where confd is a sysrepo-plugin and
    not a real daemon, which its name implies)
    
    In this patch, we break out the interface part of the operational
    sysrepo status, previously "statd", into a new daemon, also called
    "statd".
    
    The main reason for doing this is to avoid sysrepo threading, which
    allows the new "statd" to have control over sysrepo events in the
    same event pool as netlink events from the kernel. This in turn allows
    statd to subscribe and unsubscribe dynamically to interfaces that
    comes and goes during runtime (such as vlans).
    
    It also makes sense that "statd" is in fact a finit daemon.
    
    Signed-off-by: Richard Alpe <richard@bit42.se>

---
+++ package/statd/statd.mk
@@ -0,0 +1,28 @@
+################################################################################
+#
+# statd
+#
+################################################################################
+STATD_VERSION = 1.0
+STATD_LICENSE = MIT
+STATD_SITE_METHOD = local
+STATD_SITE = $(BR2_EXTERNAL_INFIX_PATH)/src/statd
+STATD_DEPENDENCIES = sysrepo libev jansson
+
+define STATD_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) \
+		LDFLAGS="$(TARGET_LDFLAGS)"
+endef
+
+define STATD_INSTALL_TARGET_CMDS
+	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) \
+		DESTDIR="$(TARGET_DIR)" install
+endef
+
+define STATD_INSTALL_EXTRA
+	cp $(STATD_PKGDIR)/statd.conf  $(FINIT_D)/available/
+	ln -sf ../available/statd.conf $(FINIT_D)/enabled/statd.conf
+endef
+STATD_TARGET_FINALIZE_HOOKS += STATD_INSTALL_EXTRA
+
+$(eval $(generic-package))
