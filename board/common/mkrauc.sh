f056e20558872842866cafd6edadc2e7946cb009
Tobias Waldekranz <tobias@waldekranz.com>
2023-06-26 22:53:51 +0200

    rauc: Bundle RAUC status file in disk image
    This let's us report the installed software versions, before any
    updates have been made. Since the info should _not_ be erased in a
    factory reset (since the installed software versions will not change),
    move the status file to aux, which is the proper place for this class
    of information.

--- board/common/mkrauc.sh
+++ board/common/mkrauc.sh
@@ -46,3 +46,34 @@ rm -f $BINARIES_DIR/$name.pkg
 
 rauc --cert=$crt --key=$key \
     bundle $work $BINARIES_DIR/$name.pkg
+
+# Bootstrap a RAUC status file showing the newly created image
+# installed to both the primary and secondary slots. This then bundled
+# in the aux partition in mkdisk.sh, so that RAUC (on the target) can
+# always report the installed versions.
+rauc info --no-verify --output-format=shell $BINARIES_DIR/$name.pkg >$work/rauc.info
+. $work/rauc.info
+tstamp=$(date -u +%FT%TZ)
+cat >$work/rauc.status <<EOF
+[slot.rootfs.0]
+bundle.compatible=$RAUC_MF_COMPATIBLE
+bundle.version=$RAUC_MF_VERSION
+status=ok
+sha256=$RAUC_IMAGE_DIGEST_0
+size=$RAUC_IMAGE_SIZE_0
+installed.timestamp=$tstamp
+installed.count=1
+activated.timestamp=$tstamp
+activated.count=1
+
+[slot.rootfs.1]
+bundle.compatible=$RAUC_MF_COMPATIBLE
+bundle.version=$RAUC_MF_VERSION
+status=ok
+sha256=$RAUC_IMAGE_DIGEST_0
+size=$RAUC_IMAGE_SIZE_0
+installed.timestamp=$tstamp
+installed.count=1
+activated.timestamp=$tstamp
+activated.count=1
+EOF
