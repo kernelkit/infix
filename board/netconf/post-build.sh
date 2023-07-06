2471a6ffcc28c32ab9c984e145df1c7f3838a484
Joachim Wiberg <troglobit@gmail.com>
2023-07-06 13:15:46 +0200

    confd: add support for specifying login shell for ietf-system users
     - Add /bin/clish (symlink) to /etc/shells, as well as the traditional
       /bin/{true,false} for completeness
     - Validate shell string, with fallback to /bin/bash and /bin/sh
     - Step infix-system revision, keeping old model in tree
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- board/netconf/post-build.sh
+++ board/netconf/post-build.sh
@@ -7,3 +7,7 @@ if [ -L "$TARGET_DIR/var/lib/avahi-autoipd" ]; then
 	rm    "$TARGET_DIR/var/lib/avahi-autoipd"
 	mkdir "$TARGET_DIR/var/lib/avahi-autoipd"
 fi
+
+# Allow clish (symlink to /usr/bin/klish) to be a login shell
+grep -qsE '^//bin/clish$$' "$TARGET_DIR/etc/shells" \
+        || echo "/bin/clish" >> "$TARGET_DIR/etc/shells"
---
+++ board/netconf/rootfs/etc/profile.d/alias.sh
@@ -0,0 +1,1 @@
+alias cli='clish'
