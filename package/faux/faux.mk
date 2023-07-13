3a4097c741e8b14f407d3fbe2c1892926146acc2
Joachim Wiberg <troglobit@gmail.com>
2023-07-13 16:52:42 +0200

    package: switch faux and klish to KernelKit mirrors on GitHub
    No need to go fishing every time we build when we have everything we
    need in the pond next door.
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- package/faux/faux.mk
+++ package/faux/faux.mk
@@ -5,7 +5,8 @@
 ################################################################################
 
 FAUX_VERSION = tags/2.1.0
-FAUX_SITE = https://src.libcode.org/pkun/faux.git
+FAUX_SITE = https://github.com/kernelkit/faux.git
+#FAUX_SITE = https://src.libcode.org/pkun/faux.git
 FAUX_SITE_METHOD = git
 FAUX_LICENSE = BSD-3
 FAUX_LICENSE_FILES = LICENCE
