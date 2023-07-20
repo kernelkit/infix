0a9c316f7d903b35e7e67b2fd1e54ea44cc4e4f8
Joachim Wiberg <troglobit@gmail.com>
2023-07-20 14:36:33 +0200

    package/faux: bump to latest upstream git hash
    This is needed for the bleeding edge klish + klish-plugin-sysrepo, which
    the next couple of commits will add.
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- package/faux/faux.mk
+++ package/faux/faux.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 
-FAUX_VERSION = tags/2.1.0
+FAUX_VERSION = 99c9cf7b95d8e2955519e2bec5ddb021eeda2d55
 FAUX_SITE = https://github.com/kernelkit/faux.git
+#FAUX_VERSION = tags/2.1.0
 #FAUX_SITE = https://src.libcode.org/pkun/faux.git
 FAUX_SITE_METHOD = git
 FAUX_LICENSE = BSD-3
