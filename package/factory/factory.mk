6ef8d227914075510a04a4632a95ace4039adbc4
Joachim Wiberg <troglobit@gmail.com>
2023-08-30 08:49:53 +0200

    package: set foo_REDISTRIBUTE = NO for local packages
    When running `make legal-info` Buildroot will collect source and
    licenses for all packages, which generates a few warnings for our
    local packages.  Local packages are skipped.
    
    > The source code for packages that set <PKG>_REDISTRIBUTE = NO will not
    > be saved.  Patches that were applied are also saved, along with a file
    > named series that lists the patches in the order they were applied.
    
    Hence, setting 'foo_REDISTRIBUTE = NO' disables the warning, which seems
    like a good idea to reduce the number of unnecessary questions we might
    otherwise get.
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- package/factory/factory.mk
+++ package/factory/factory.mk
@@ -3,11 +3,13 @@
 # factory
 #
 ################################################################################
+
 FACTORY_VERSION = 1.0
 FACTORY_LICENSE = MIT
 FACTORY_LICENSE_FILES = LICENSE
 FACTORY_SITE_METHOD = local
 FACTORY_SITE = $(BR2_EXTERNAL_INFIX_PATH)/src/factory
+FACTORY_REDISTRIBUTE = NO
 
 define FACTORY_BUILD_CMDS
 	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) \
