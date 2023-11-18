cc6232bf3834b2ce7fe25a0f93c84a95589b8828
Joachim Wiberg <troglobit@gmail.com>
2023-11-18 14:46:16 +0100

    package/iito: relocate Finit conf and add tmpfiles.d for /run/led
    Relocate Finit conf to package, like klish and confd.  Add condition to
    ensure it is not started before mdev/udevd are up and kernel LED modules
    have been loaded properly.
    
    Also, add tmpfiles.conf to ensure /run/led is recreated at every boot.
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- package/iito/iito.mk
+++ package/iito/iito.mk
@@ -10,4 +10,14 @@ IITO_LICENSE = GPL-2.0
 IITO_LICENSE_FILES = COPYING
 IITO_DEPENDENCIES = jansson libev udev
 
+define IITO_INSTALL_HOOK
+	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_INFIX_PATH)/package/iito/iitod.svc \
+		$(FINIT_D)/available/iitod.conf
+	$(INSTALL) -d -m 0755 $(FINIT_D)/enabled
+	ln -sf ../available/iitod.conf $(FINIT_D)/enabled/iitod.conf
+	cp $(CONFD_PKGDIR)/tmpfiles.conf $(TARGET_DIR)/etc/tmpfiles.d/iitod.conf
+endef
+
+IITO_POST_INSTALL_TARGET_HOOKS += IITO_INSTALL_HOOK
+
 $(eval $(autotools-package))
