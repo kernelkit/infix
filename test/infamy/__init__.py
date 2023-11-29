ad97d436ca3adf05a586b1f76da5433c94733a5e
Tobias Waldekranz <tobias@waldekranz.com>
2023-11-29 16:45:44 +0100

    test/infamy: Generalize wait functionality into "until"

--- test/infamy/__init__.py
+++ test/infamy/__init__.py
@@ -4,6 +4,7 @@ from .env import Env
 from .netns import IsolatedMacVlan
 from .sniffer import Sniffer
 from .tap import Test
+from .util import until
 
 def std_topology(name):
     return os.path.realpath(
