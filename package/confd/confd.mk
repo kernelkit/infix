3382a0471bf698af342b96db24494e9be13fbbad
Joachim Wiberg <troglobit@gmail.com>
2023-11-29 07:17:17 +0100

    package/confd: install avahi _netconf-ssh._tcp service record
    Currently we only advertise SSH+SFTP services over mDNS-SD, which are
    the default services from the avahi package.  This patch adds support
    for _netconf-ssh._tcp as well.
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- package/confd/confd.mk
+++ package/confd/confd.mk
@@ -17,6 +17,8 @@ define CONFD_INSTALL_EXTRA
 	cp $(CONFD_PKGDIR)/confd.conf  $(FINIT_D)/available/
 	ln -sf ../available/confd.conf $(FINIT_D)/enabled/confd.conf
 	cp $(CONFD_PKGDIR)/tmpfiles.conf $(TARGET_DIR)/etc/tmpfiles.d/confd.conf
+	mkdir -p $(TARGET_DIR)/etc/avahi/services
+	cp $(CONFD_PKGDIR)/avahi.service $(TARGET_DIR)/etc/avahi/services/netconf.service
 endef
 CONFD_POST_INSTALL_TARGET_HOOKS += CONFD_INSTALL_EXTRA
 
