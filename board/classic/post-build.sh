40c42c8fc1aec3e3179e18cb5d67e73851a5c279
Joachim Wiberg <troglobit@gmail.com>
2023-12-26 22:46:52 +0100

    dnsmasq.conf: D-Bus is not enabled in Classic builds
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- board/classic/post-build.sh
+++ board/classic/post-build.sh
@@ -6,3 +6,8 @@ if [ -L "$TARGET_DIR/etc/dropbear" ]; then
 	rm    "$TARGET_DIR/etc/dropbear"
 	mkdir "$TARGET_DIR/etc/dropbear"
 fi
+
+# Classic builds don't have D-Bus
+if [ -f "$TARGET_DIR/etc/dnsmasq.conf" ]; then
+    sed -i '/enable-dbus/d' "$TARGET_DIR/etc/dnsmasq.conf"
+fi
