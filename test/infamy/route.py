3d90760f7fb03d7f8797512b2f16c2b4b7d2a6c4
Mattias Walström <lazzer@gmail.com>
2024-01-05 16:59:19 +0100

    Operational: Add support for multiple next-hop for routes

--- test/infamy/route.py
+++ test/infamy/route.py
@@ -7,18 +7,29 @@ def _get_routes(target, protocol):
         return r.get("routes", {}).get("route",{})
     return {}
 
-def _exist_route(target, prefix, nexthop, version,source_protocol):
+def _exist_route(target, prefix, nexthop, version, source_protocol):
     routes = _get_routes(target, version)
     for r in routes:
         if r["destination-prefix"] != prefix:
             continue
-        if source_protocol and r.get("source-protocol") != source_protocol:
-            continue
+
+        if not nexthop and not source_protocol: # Only want the route to exist
+            return True
+
+        if source_protocol and r.get("source-protocol") == source_protocol:
+            return True
 
         nh = r["next-hop"]
-        if nexthop and nh["next-hop-address"] != nexthop:
-            continue
-        return True
+        next_hop_list = nh.get("next-hop-list")
+        if nexthop:
+            if next_hop_list:
+                for nhl in next_hop_list["next-hop"]:
+                    address = nhl.get("address")
+                    if address == nexthop:
+                        return True
+            else:
+                if nh["next-hop-address"] == nexthop:
+                    return True
     return False
 
 def ipv4_route_exist(target, prefix, nexthop=None,source_protocol=None):
