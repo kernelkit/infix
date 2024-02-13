15b32cfcc29daaa52d12c360121de082f130847b
Mattias Walström <lazzer@gmail.com>
2024-02-13 16:56:59 +0100

    infamy: Add a simple USB test

---
+++ test/case/ietf_hardware/usb.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+
+import infamy
+import copy
+import infamy.usb as usb
+from infamy.util import until
+
+with infamy.Test() as test:
+    with test.step("Initialize"):
+        env = infamy.Env(infamy.std_topology("1x1"))
+        target = env.attach("target", "mgmt")
+        available=usb.get_usb_ports(target)
+        if len(available) < 1:
+            test.skip()
+
+    with test.step("Lock USB ports"):
+        components=[]
+        for port in available:
+            component = {
+                "name": port,
+                "state": {
+                    "admin-state": "locked"
+                }
+            }
+            components.append(component)
+
+        target.put_config_dict("ietf-hardware", {
+            "hardware": {
+                "component": components
+            }
+        })
+
+    with test.step("Verify USB ports locked"):
+        for port in available:
+            until(lambda: usb.get_usb_state(target, port) == "locked")
+
+
+    with test.step("Unlock USB ports"):
+        components=[]
+        for port in available:
+            component = {
+                "name": port,
+                "state": {
+                    "admin-state": "unlocked"
+                }
+            }
+            components.append(component)
+
+        target.put_config_dict("ietf-hardware", {
+            "hardware": {
+                "component": components
+            }
+        })
+
+
+    with test.step("Verify USB ports unlocked"):
+        for port in available:
+            until(lambda: usb.get_usb_state(target, port) == "unlocked")
+
+    if len(available) > 1:
+        with test.step("Lock one port"):
+            components=[]
+            component = {
+                "name": available[1],
+                "class": "infix-hardware:usb",
+                "state": {
+                    "admin-state": "locked"
+                }
+            }
+            components.append(component)
+
+        target.put_config_dict("ietf-hardware", {
+            "hardware": {
+                "component": components
+            }
+        })
+        with test.step("Verify one port is locked and one unlocked"):
+            until(lambda: usb.get_usb_state(target, available[1]) == "locked")
+            until(lambda: usb.get_usb_state(target, available[0]) == "unlocked")
+
+    with test.step("Remove all hardware configuration"):
+            running = target.get_config_dict("/ietf-hardware:hardware")
+            new = copy.deepcopy(running)
+            new["hardware"].clear()
+            target.put_diff_dicts("ietf-hardware",running,new)
+
+    with test.step("Verify USB ports locked"):
+        for port in available:
+            until(lambda: usb.get_usb_state(target, port) == "locked")
+
+
+    with test.step("Unlock USB ports"):
+        components=[]
+        for port in available:
+            component = {
+                "name": port,
+                "class": "infix-hardware:usb",
+                "state": {
+                    "admin-state": "unlocked"
+                }
+            }
+            components.append(component)
+
+        target.put_config_dict("ietf-hardware", {
+            "hardware": {
+                "component": components
+            }
+        })
+
+    with test.step("Verify USB ports unlocked"):
+        for port in available:
+            until(lambda: usb.get_usb_state(target, port) == "unlocked")
+
+    test.succeed()
