6fa1eb9e008afe07fbcb842b36f86e2497e543cd
Joachim Wiberg <troglobit@gmail.com>
2024-02-19 06:56:20 +0100

    package/k8s-logger: new package
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

---
+++ package/k8s-logger/k8s-logger.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# k8s-logger
+#
+################################################################################
+
+K8S_LOGGER_VERSION = 1.2
+K8S_LOGGER_SITE = https://github.com/kernelkit/k8s-logger/releases/download/v$(K8S_LOGGER_VERSION)
+K8S_LOGGER_LICENSE = MIT
+K8S_LOGGER_LICENSE_FILES = LICENSE
+K8S_LOGGER_DEPENDENCIES = sysklogd libite
+K8S_LOGGER_CONF_OPTS = --with-syslogp
+
+$(eval $(autotools-package))
