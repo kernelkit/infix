4869b73b1488e37892329f812dd13fc83f1cf6eb
Mattias Walström <lazzer@gmail.com>
2024-02-21 14:38:01 +0100

    test: usb: test startup with no hardware configuration
    In this case the USB ports shall be locked.

--- test/case/ietf_hardware/usb.py
+++ test/case/ietf_hardware/usb.py
@@ -3,13 +3,22 @@
 import infamy
 import copy
 import infamy.usb as usb
+import time
+import infamy.netconf as netconf
+
 from infamy.util import until
+def remove_config(target):
+    running = target.get_config_dict("/ietf-hardware:hardware")
+    new = copy.deepcopy(running)
+    new["hardware"].clear()
+    target.put_diff_dicts("ietf-hardware",running,new)
 
 with infamy.Test() as test:
     with test.step("Initialize"):
         env = infamy.Env(infamy.std_topology("1x1"))
         target = env.attach("target", "mgmt")
         available=usb.get_usb_ports(target)
+
         if len(available) < 1:
             test.skip()
 
@@ -79,10 +88,7 @@ with infamy.Test() as test:
             until(lambda: usb.get_usb_state(target, available[0]) == "unlocked")
 
     with test.step("Remove all hardware configuration"):
-            running = target.get_config_dict("/ietf-hardware:hardware")
-            new = copy.deepcopy(running)
-            new["hardware"].clear()
-            target.put_diff_dicts("ietf-hardware",running,new)
+        remove_config(target)
 
     with test.step("Verify USB ports locked"):
         for port in available:
@@ -111,4 +117,22 @@ with infamy.Test() as test:
         for port in available:
             until(lambda: usb.get_usb_state(target, port) == "unlocked")
 
+    with test.step("Remove USB configuration, and reboot"):
+        remove_config(target)
+        target.copy("running", "startup")
+        target.reboot()
+        until(lambda: target.reachable() == False, attempts = 100)
+        print("Device reboots..")
+        until(lambda: target.reachable() == True, attempts = 300)
+        print("Device has come online")
+        iface=target.get_mgmt_iface()
+        neigh=infamy.neigh.ll6ping(iface)
+        assert(neigh)
+        until(lambda: netconf.netconf_syn(neigh) == True, attempts = 300)
+        print("NETCONF reachable")
+        target = env.attach("target", "mgmt", factory_default = False)
+
+    with test.step("Verify that all ports are locked"):
+        for port in available:
+            until(lambda: usb.get_usb_state(target, port) == "locked")
     test.succeed()
