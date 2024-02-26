fcce0aa54f2d7760198353b0471e02720e406441
Mattias Walström <lazzer@gmail.com>
2024-02-26 10:01:52 +0100

    Add new function that only waits for boot (reboot) to complete
    This since it can take an undefined time to reboot the DUT.

--- test/infamy/util.py
+++ test/infamy/util.py
@@ -1,4 +1,6 @@
 import time
+import infamy.neigh
+import infamy.netconf as netconf
 
 def until(fn, attempts=10, interval=1):
     for attempt in range(attempts):
@@ -8,3 +10,16 @@ def until(fn, attempts=10, interval=1):
         time.sleep(interval)
 
     raise Exception("Expected condition did not materialize")
+
+def wait_boot(target):
+    until(lambda: target.reachable() == False, attempts = 100)
+    print("Device is booting..")
+    until(lambda: target.reachable() == True, attempts = 300)
+    iface=target.get_mgmt_iface()
+    if not iface:
+        return False
+    neigh=infamy.neigh.ll6ping(iface)
+    if not neigh:
+        return False
+    until(lambda: netconf.netconf_syn(neigh) == True, attempts = 300)
+    return True
