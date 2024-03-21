793cff3d2f9a7ab1db07b3fbb34037af7037930b
Joachim Wiberg <troglobit@gmail.com>
2024-03-21 06:48:42 +0100

    package/mcd: bump to v2.0
    For details, see https://github.com/kernelkit/mcd/releases/tag/v2.0
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- package/mcd/mcd.mk
+++ package/mcd/mcd.mk
@@ -4,14 +4,14 @@
 #
 ################################################################################
 
-MCD_VERSION = 1.0
+MCD_VERSION = 2.0
 MCD_SITE    = https://github.com/kernelkit/mcd/releases/download/v$(MCD_VERSION)
 MCD_LICENSE = BSD-3-Clause
 MCD_LICENSE_FILES = LICENSE
 
 define MCD_INSTALL_CONFIG
-	$(INSTALL) -D -m 0644 $(MCD_PKGDIR)/example.conf \
-		$(TARGET_DIR)/etc/mcd/example.conf
+	$(INSTALL) -D -m 0644 $(@D)/mcd.conf $(TARGET_DIR)/etc/mcd.conf
+	$(INSTALL) -d -m 0755 $(TARGET_DIR)/etc/mc.d
 endef
 MCD_POST_INSTALL_TARGET_HOOKS += MCD_INSTALL_CONFIG
 
