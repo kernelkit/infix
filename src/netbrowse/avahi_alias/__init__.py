deeb39840ac46175d9f33ea60a423943ba6b2b68
Joachim Wiberg <troglobit@gmail.com>
2024-03-25 19:40:50 +0100

    package/netbrowse: new package
     - Advertises the build-time $hostname.local as a CNAME to the A and
       AAAA records already advertised by Avahi
     - Advertises a special network.local CNAME and provides a fastcgi
       service on unix:/tmp/netbrowse.sock for browsing mDNS services
    
    This commit also activates netbrowse by default with nginx listeing
    to port 80 on IPv4 and IPv6 /browse by default but also / if called
    with server name network.local.
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

---
+++ src/netbrowse/avahi_alias/__init__.py
@@ -0,0 +1,6 @@
+"""
+    Avahi CNAME service over D-Bus advertiser
+"""
+
+from .avahi_alias import AvahiAlias
+__all__ = ['AvahiAlias']
---
+++ src/netbrowse/mdns_hosts/__init__.py
@@ -0,0 +1,6 @@
+"""
+    Very basic mDNS scanner with HTML table renderer
+"""
+
+from .mdns_hosts import MdnsHosts
+__all__ = ['MdnsHosts']
