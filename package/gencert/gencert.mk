6b8bd4caf3408105e13009dddbf9bc773564df3c
Joachim Wiberg <troglobit@gmail.com>
2024-05-03 14:30:05 +0200

    package/gencert: new package
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

---
+++ package/gencert/gencert.mk
@@ -0,0 +1,22 @@
+################################################################################
+#
+# gencert
+#
+################################################################################
+
+GENCERT_VERSION = 1.0
+GENCERT_SITE_METHOD = local
+GENCERT_SITE = $(BR2_EXTERNAL_INFIX_PATH)/src/gencert
+GENCERT_LICENSE = ISC
+GENCERT_LICENSE_FILES = LICENSE
+GENCERT_REDISTRIBUTE = NO
+GENCERT_DEPENDENCIES = openssl
+GENCERT_AUTORECONF = YES
+
+define GENCERT_CONF_ENV
+CFLAGS="$(INFIX_CFLAGS)"
+endef
+
+GENCERT_CONF_OPTS = --prefix= --disable-silent-rules
+
+$(eval $(autotools-package))
