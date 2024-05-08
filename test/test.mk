2c72aaf6e1004ed80246b852986f78c2bfe21177
Tobias Waldekranz <tobias@waldekranz.com>
2024-05-08 10:46:58 +0000

    test: Move all test related targets from board/x86_64 to test/
    The list of binaries needed inside the test environment is the only
    architecture specific bit, so refactor that out to a separate
    variable.

--- test/test.mk
+++ test/test.mk
@@ -1,8 +1,36 @@
 test-dir := $(BR2_EXTERNAL_INFIX_PATH)/test
 
-UNIT_TESTS ?= $(test-dir)/case/all-repo.yaml $(test-dir)/case/all-unit.yaml
+UNIT_TESTS  ?= $(test-dir)/case/all-repo.yaml $(test-dir)/case/all-unit.yaml
+INFIX_TESTS ?= $(test-dir)/case/all.yaml
+
+binaries-$(ARCH) := $(addprefix infix-$(ARCH),.img -disk.img .pkg)
+binaries-x86_64  += OVMF.fd
+binaries := $(foreach bin,$(binaries-$(ARCH)),-f $(BINARIES_DIR)/$(bin))
+
+test-env-qeneth = $(test-dir)/env $(binaries) -q $(test-dir)/virt/quad $(1)
+test-env-run    = $(test-dir)/env $(binaries) -C -t $(BINARIES_DIR)/qemu.dot $(1)
+
+test test-qeneth:
+	$(call test-env-qeneth,\
+		$(BR2_EXTERNAL_INFIX_PATH)/test/9pm/9pm.py \
+			$(INFIX_TESTS))
+test-sh test-qeneth-sh:
+	$(call test-env-qeneth,-i /bin/sh)
+
+test-run: | ~/.infix-test-venv
+	$(call test-env-run,\
+		$(BR2_EXTERNAL_INFIX_PATH)/test/9pm/9pm.py \
+			$(INFIX_TESTS))
+test-run-sh: | ~/.infix-test-venv
+	$(call test-env-run,/bin/sh)
+
+test-run-play: | ~/.infix-test-venv
+	$(call test-env-run,$(test-dir)/case/meta/play.py)
+
+~/.infix-test-venv:
+	$(test-dir)/docker/init-venv.sh $(test-dir)/docker/pip-requirements.txt
 
 test-unit:
 	$(test-dir)/env $(test-dir)/9pm/9pm.py $(UNIT_TESTS)
 
-.PHONY: test-unit
+.PHONY: test test-sh test-qeneth test-qeneth-sh test-run test-run-sh test-run-play test-unit
