eb1c01edea3c0e837c83359262b2c466c9fb529a
Joachim Wiberg <troglobit@gmail.com>
2024-05-08 14:04:53 +0200

    confd: fix regression, loss of admin account after upgrade to v24.04
    Fix regression introduced in 3f87945, which tries to map a user to an
    existing home directory.  The root cause for the problem is basically an
    invalid check of the /home/admin UID being in use by any system account
    or not.  This patch uses the same check used by the BusyBox adduser
    command.
    
    In addition to the uid-already-in-use fix, several other checks have
    been added to ensure we do not end up with an invalid system state:
    
     - If reusing an existing /home/admin fails in the 'adduser admin'
       command, wipe out /home/admin and retry adding the user cleanly
     - Always delete any group with the same name before trying to add
       the user, regardless if /home/$user exists or not, this prevents
       issues with stale group records if we end up failing to load the
       startup-config and need to load failure-config
     - For the same reason (failure-config), make sure to always clean
       up any stale user in the retry step.  The same step is used when
       there is a uid:gid mismapping (the original bug)
    
    Fixes #428
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- src/libsrx/src/common.h
+++ src/libsrx/src/common.h
@@ -20,12 +20,13 @@ int asprintf(char **strp, const char *fmt, ...);
 
 /*
  * Only NOTICE and above are logged by default, see setlogmask() for details.
+ * Use `set DEBUG=1` at top of package/confd/confd.conf for logmask = debug
  */
 #define DEBUG(fmt, ...) do { if (debug) syslog(LOG_DEBUG, fmt, ##__VA_ARGS__); } while (0)
 #define INFO(fmt, ...)  syslog(LOG_INFO, fmt, ##__VA_ARGS__)
 #define NOTE(fmt, ...)  syslog(LOG_NOTICE, fmt, ##__VA_ARGS__)
-#define WARN(fmt, ...)  syslog(LOG_WARNING, "%s: " fmt, __func__, ##__VA_ARGS__)
-#define ERROR(fmt, ...) syslog(LOG_ERR, "%s: " fmt, __func__, ##__VA_ARGS__)
-#define ERRNO(fmt, ...) syslog(LOG_ERR, "%s: " fmt ": %s", __func__, ##__VA_ARGS__, strerror(errno))
+#define WARN(fmt, ...)  syslog(LOG_WARNING, fmt, ##__VA_ARGS__)
+#define ERROR(fmt, ...) syslog(LOG_ERR, fmt, ##__VA_ARGS__)
+#define ERRNO(fmt, ...) syslog(LOG_ERR, fmt ": %s", ##__VA_ARGS__, strerror(errno))
 
 #endif	/* CONFD_COMMON_H_ */
