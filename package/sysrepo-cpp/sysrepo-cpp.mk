d2d1562c8c08732e60317f6a90d51ea354c5f67d
Mattias Walström <lazzer@gmail.com>
2024-06-04 09:43:15 +0200

    sysrepo-cpp: Change to kernelkit-url

--- package/sysrepo-cpp/sysrepo-cpp.mk
+++ package/sysrepo-cpp/sysrepo-cpp.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 SYSREPO_CPP_VERSION = 99747f74e57a09c664251ea1f4e059e3f4f8e66a
-SYSREPO_CPP_SITE = git@github.com:sysrepo/sysrepo-cpp.git
+SYSREPO_CPP_SITE = https://github.com/kernelkit/sysrepo-cpp.git
 SYSREPO_CPP_SITE_METHOD = git
 SYSREPO_CPP_LICENSE = BSD-3-Clause
 SYSREPO_CPP_LICENSE_FILES = LICENCE
