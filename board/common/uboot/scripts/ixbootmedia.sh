ccb17b1c91cd96b4f3e7e26685c69f06b0048fdc
Tobias Waldekranz <tobias@waldekranz.com>
2024-06-07 15:55:53 +0200

    uboot: Remove dead code

--- board/common/uboot/scripts/ixbootmedia.sh
+++
@@ -1,6 +0,0 @@
-echo "Booting from ${devtype}${devnum} (order: ${BOOT_ORDER})"
-
-for s in "${BOOT_ORDER}"; do
-    setenv slot "${s}"
-    run ixbootslot
-done
--- board/common/uboot/scripts/prepare_aux_mmc.sh
+++
@@ -1,5 +0,0 @@
-if part number mmc 0 aux auxpart; then
-    if load mmc 0:${auxpart} ${loadaddr} /uboot.env; then
-	env import -b ${loadaddr} ${filesize} BOOT_ORDER DEBUG
-    fi
-fi
