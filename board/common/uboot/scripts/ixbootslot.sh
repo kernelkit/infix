34a6a0ecc48a9962ba4c0a274af7b31b7808077e
Tobias Waldekranz <tobias@waldekranz.com>
2024-06-17 08:39:05 +0200

    uboot: Allow untrusted images to boot in developer mode
    Since a user has access to the shell in developer mode, we might as
    well allow booting through the standard boot scripts as well. This
    means that a product with a bootloader only accepting images signed by
    Company A can now be configured to netboot a regular developer image
    without any special user interaction.

--- board/common/uboot/scripts/ixbootslot.sh
+++ board/common/uboot/scripts/ixbootslot.sh
@@ -10,34 +10,42 @@ elif test "${slot}" = "net"; then
 fi
 
 if test "${prepared}" = "ok"; then
-	echo "${slot}: Validating..."
-	if iminfo ${ramdisk_addr_r}; then
-	    echo "${slot}: Booting..."
+    echo "${slot}: Validating..."
 
-	    setenv bootargs_user "rauc.slot=${slot}"
-	    if test "${factory_reset}" = "yes"; then
-		setenv bootargs_user "${bootargs_user} finit.cond=factory-reset"
-	    fi
-	    if test "${dev_mode}" = "yes"; then
-		setenv bootargs_user "${bootargs_user} finit.cond=dev-mode"
-	    fi
+    iminfo ${ramdisk_addr_r}
+    imok=$?
 
-	    blkmap create boot
-	    blkmap get boot dev blkmapnum
-	    blkmap map boot 0 ${sqblkcnt} mem ${sqaddr}
-
-	    for conf in "${board}-syslinux.conf syslinux.conf"; do
-		if test -e blkmap ${blkmapnum} /boot/syslinux/${conf}; then
-		    sysboot blkmap ${blkmapnum} any ${scriptaddr} /boot/syslinux/${conf}
-		fi
-	    done
-
-	    blkmap destroy boot
-
-	    echo "${slot}: ERROR: Boot failed"
-	else
-	    echo "${slot}: ERROR: Validation failed"
+    if test ${imok} -eq 0 || test "${dev_mode}" = "yes"; then
+	if test ${imok} -ne 0; then
+	    echo "DEVELOPER MODE ACTIVE, BOOTING UNTRUSTED IMAGE"
 	fi
+
+	echo "${slot}: Booting..."
+
+	setenv bootargs_user "rauc.slot=${slot}"
+	if test "${factory_reset}" = "yes"; then
+	    setenv bootargs_user "${bootargs_user} finit.cond=factory-reset"
+	fi
+	if test "${dev_mode}" = "yes"; then
+	    setenv bootargs_user "${bootargs_user} finit.cond=dev-mode"
+	fi
+
+	blkmap create boot
+	blkmap get boot dev blkmapnum
+	blkmap map boot 0 ${sqblkcnt} mem ${sqaddr}
+
+	for conf in "${board}-syslinux.conf syslinux.conf"; do
+	    if test -e blkmap ${blkmapnum} /boot/syslinux/${conf}; then
+		sysboot blkmap ${blkmapnum} any ${scriptaddr} /boot/syslinux/${conf}
+	    fi
+	done
+
+	blkmap destroy boot
+
+	echo "${slot}: ERROR: Boot failed"
+    else
+	echo "${slot}: ERROR: Validation failed"
+    fi
 else
-	echo "${slot}: ERROR: Unable to use slot"
+    echo "${slot}: ERROR: Unable to use slot"
 fi
