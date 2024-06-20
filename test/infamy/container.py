eb4ee4917d1464fb92c642289de2a42b63424dac
Joachim Wiberg <troglobit@gmail.com>
2024-06-20 09:44:51 +0200

    package/curios-httpd: bump to v24.05.0
     - Fixes to missing BusyBox tools (cat)
     - Bump Buildroot to this year's LTS (2024.02)
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- test/infamy/container.py
+++ test/infamy/container.py
@@ -2,7 +2,7 @@
 
 class Container:
     """Helper methods"""
-    IMAGE = "curios-httpd-v24.03.0.tar.gz"
+    IMAGE = "curios-httpd-v24.05.0.tar.gz"
 
     def __init__(self, target):
         self.system = target
