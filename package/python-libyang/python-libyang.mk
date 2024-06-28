31a30c736a3faf408e7012407318814f5b28f635
Joachim Wiberg <troglobit@gmail.com>
2024-06-28 07:50:20 +0200

    package/python-libyang: new package
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

---
+++ package/python-libyang/python-libyang.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# python-libyang
+#
+################################################################################
+
+PYTHON_LIBYANG_VERSION = 3.0.1
+PYTHON_LIBYANG_SOURCE = libyang-$(PYTHON_LIBYANG_VERSION).tar.gz
+PYTHON_LIBYANG_SITE = https://files.pythonhosted.org/packages/91/2e/ff13ee16c874d232d5d3fdff83f629cbc9ac47f9aaf2b59256b6a1bdbc16
+PYTHON_LIBYANG_SETUP_TYPE = setuptools
+PYTHON_LIBYANG_LICENSE = MIT
+PYTHON_LIBYANG_LICENSE_FILES = LICENSE
+HOST_PYTHON_LIBYANG_DEPENDENCIES = host-python-cython host-python-cffi
+
+$(eval $(host-python-package))
