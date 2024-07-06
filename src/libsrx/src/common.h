a61bafe0ed3551217b8eeeb3ec6f662df314a1ab
Joachim Wiberg <troglobit@gmail.com>
2024-07-06 13:18:21 +0200

    confd: refactor user hash validation
    Consolidate empty password and $factory$ hash extraction to a common
    function called before set_pasword().  The purpose is to ensure all
    error handling and log messages are in the same place, except for
    missing factory-password-hash, which is now treated as a PANIC and
    logged to /dev/console.
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- src/libsrx/src/common.h
+++ src/libsrx/src/common.h
@@ -26,6 +26,7 @@ int asprintf(char **strp, const char *fmt, ...);
 #define INFO(fmt, ...)  syslog(LOG_INFO, fmt, ##__VA_ARGS__)
 #define NOTE(fmt, ...)  syslog(LOG_NOTICE, fmt, ##__VA_ARGS__)
 #define WARN(fmt, ...)  syslog(LOG_WARNING, fmt, ##__VA_ARGS__)
+#define EMERG(fmt, ...) syslog(LOG_EMERG, fmt, ##__VA_ARGS__)
 #define ERROR(fmt, ...) syslog(LOG_ERR, fmt, ##__VA_ARGS__)
 #define ERRNO(fmt, ...) syslog(LOG_ERR, fmt ": %s", ##__VA_ARGS__, strerror(errno))
 
