67d1d159c137596d13c38303763414f172e878d3
Joachim Wiberg <troglobit@gmail.com>
2024-07-10 05:29:48 +0200

    package/python-yangdoc: bump to v0.6.0
    Fixes #529
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- package/python-yangdoc/python-yangdoc.mk
+++ package/python-yangdoc/python-yangdoc.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_YANGDOC_VERSION = 0.4.0
+PYTHON_YANGDOC_VERSION = 0.6.0
 PYTHON_YANGDOC_SOURCE = yangdoc-$(PYTHON_YANGDOC_VERSION).tar.gz
-PYTHON_YANGDOC_SITE = https://files.pythonhosted.org/packages/75/83/a426dcb6f9c56b3cfc70c49040a7ea76c6e9be8ed86026eb6b5942dcb03a
+PYTHON_YANGDOC_SITE = https://files.pythonhosted.org/packages/a7/35/b9770953d7e42a6e2a2f0a88f79656610f4e36b33946cd957c5d7d55824d
 PYTHON_YANGDOC_SETUP_TYPE = setuptools
 PYTHON_YANGDOC_LICENSE = MIT
 PYTHON_YANGDOC_LICENSE_FILES = LICENSE
