ee6d96d3ed23b50ae627162f9acb88d42baa95d0
Joachim Wiberg <troglobit@gmail.com>
2024-08-28 10:43:49 +0200

    test: adjust location for Infamy .venv directory to ~/.infix/venv
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- test/docker/init-venv.sh
+++ test/docker/init-venv.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
+# shellcheck disable=SC1090
 
 set -e
 
-python3 -m venv ~/.infix-test-venv
-cp -r  ~/yang ~/.infix-test-venv/yangdir
-. ~/.infix-test-venv/bin/activate
+mkdir -p        ~/.infix/venv
+python3 -m venv ~/.infix/venv
+cp -r ~/yang    ~/.infix/venv/yangdir
+.               ~/.infix/venv/bin/activate
 
 python3 -m pip install --upgrade pip
 python3 -m pip install -r "$1"
