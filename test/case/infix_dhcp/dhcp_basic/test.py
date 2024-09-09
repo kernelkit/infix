b963788fbcdfc26472e5abbdf5cee9b8c2b3fd53
Ahmed Karic <ahmkar@addiva.se>
2024-09-09 15:39:08 +0200

    test: update infix_dhcp test topologies

--- test/case/infix_dhcp/dhcp_basic/test.py
+++ test/case/infix_dhcp/dhcp_basic/test.py
@@ -18,13 +18,13 @@ with infamy.Test() as test:
     ADDRESS = '10.0.0.42'
     with test.step("Initialize"):
         env = infamy.Env()
-        target = env.attach("target", "mgmt")
+        client = env.attach("client", "mgmt")
         _, host = env.ltop.xlate("host", "data")
 
     with infamy.IsolatedMacVlan(host) as netns:
         netns.addip("10.0.0.1")
         with infamy.dhcp.Server(netns, ip=ADDRESS):
-            _, port = env.ltop.xlate("target", "data")
+            _, port = env.ltop.xlate("client", "data")
             config = {
                 "dhcp-client": {
                     "client-if": [{
@@ -32,9 +32,9 @@ with infamy.Test() as test:
                     }]
                 }
             }
-            target.put_config_dict("infix-dhcp-client", config)
+            client.put_config_dict("infix-dhcp-client", config)
 
             with test.step(f"Waiting for client to set DHCP lease {ADDRESS}"):
-                until(lambda: iface.address_exist(target, port, ADDRESS))
+                until(lambda: iface.address_exist(client, port, ADDRESS))
 
     test.succeed()
--- test/case/infix_dhcp/dhcp_routes/test.py
+++ test/case/infix_dhcp/dhcp_routes/test.py
@@ -17,13 +17,13 @@ with infamy.Test() as test:
     ROUTER  = '192.168.0.254'
     with test.step("Initialize"):
         env = infamy.Env()
-        target = env.attach("target", "mgmt")
+        client = env.attach("client", "mgmt")
         _, host = env.ltop.xlate("host", "data")
 
     with infamy.IsolatedMacVlan(host) as netns:
         netns.addip("192.168.0.1")
         with infamy.dhcp.Server(netns, prefix=PREFIX, router=ROUTER):
-            _, port = env.ltop.xlate("target", "data")
+            _, port = env.ltop.xlate("client", "data")
             config = {
                 "dhcp-client": {
                     "client-if": [{
@@ -35,13 +35,13 @@ with infamy.Test() as test:
                     }]
                 }
             }
-            target.put_config_dict("infix-dhcp-client", config)
+            client.put_config_dict("infix-dhcp-client", config)
 
             with test.step(f"Verify client sets up correct route via {ROUTER}"):
                 # Wait for client to set the classless static route, option 121
-                until(lambda: route.ipv4_route_exist(target, PREFIX, ROUTER))
+                until(lambda: route.ipv4_route_exist(client, PREFIX, ROUTER))
                 # Ensure client did *not* use option 3 (option 121 takes precedence)
-                if route.ipv4_route_exist(target, "0.0.0.0/0", ROUTER):
+                if route.ipv4_route_exist(client, "0.0.0.0/0", ROUTER):
                     test.fail()
 
     test.succeed()
