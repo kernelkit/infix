900c5b0b2f453a48982a1250fb000014dfaf6d70
Mattias Walström <lazzer@gmail.com>
2024-09-11 21:12:06 +0200

    test: case: ietf_system: Update test texts
    And some changes in test semantics.

--- test/case/ietf_system/hostname/test.py
+++ test/case/ietf_system/hostname/test.py
@@ -2,7 +2,7 @@
 """
 Set hostname
 
-Verify that it is possible to change hostname
+Verify that it is possible to change hostname.
 """
 import random
 import string
@@ -10,33 +10,32 @@ import re
 import infamy
 
 with infamy.Test() as test:
-    with test.step("Initialize"):
+    with test.step("Connect to device"):
         env = infamy.Env()
         target = env.attach("target", "mgmt")
         tgtssh = env.attach("target", "mgmt", "ssh")
         fmt = "%h-%m"
+        new="h0stn4m3"
 
-    with test.step("Set new hostname"):
-        new = "".join((random.choices(string.ascii_lowercase, k=16)))
-
+    with test.step("Set hostname to 'h0stn4m3'"):
         target.put_config_dict("ietf-system", {
             "system": {
                 "hostname": new,
             }
         })
 
-    with test.step(f"Verify new hostname ({new})"):
+    with test.step(f"Verify new hostname 'h0stn4m3'"):
         running = target.get_config_dict("/ietf-system:system")
         assert running["system"]["hostname"] == new
 
-    with test.step(f"Set hostname format: {fmt}"):
+    with test.step(f"Set hostname format: %h-%m"):
         target.put_config_dict("ietf-system", {
             "system": {
                 "hostname": fmt,
             }
         })
 
-    with test.step(f"Verify hostname format in running: {fmt}"):
+    with test.step(f"Verify hostname format in running: %h-%m"):
         running = target.get_config_dict("/ietf-system:system")
         if running["system"]["hostname"] != fmt:
             test.fail()
@@ -44,7 +43,7 @@ with infamy.Test() as test:
         cmd = tgtssh.runsh("sed -n s/^DEFAULT_HOSTNAME=//p /etc/os-release")
         default = cmd.stdout.rstrip()
 
-    with test.step(f"Verify hostname format in operational: {default}-c0-ff-ee"):
+    with test.step(f"Verify hostname format in operational, according to format"):
 
         oper = target.get_data("/ietf-system:system")
         name = oper["system"]["hostname"]
--- test/case/ietf_system/user_admin/test.py
+++ test/case/ietf_system/user_admin/test.py
@@ -2,18 +2,17 @@
 """
 Add admin user
 
-- Add a non-admin user and verify they have no privs
-- Add user to admin group and recheck privileges
-- Test admin user, verify $factory$ password in running
-    and active password in operational datastore
+Test that a non-admin user is not an admin in Linux, and
+check that it when added as admin it is also the case in Linux.
 """
 
 import infamy
 import infamy.ssh as ssh
+import infamy.util as util
 from passlib.hash import sha256_crypt
 
 with infamy.Test() as test:
-    with test.step("Initializing ..."):
+    with test.step("Connect to device"):
         env = infamy.Env()
         target = env.attach("target", "mgmt")
         tgtssh = env.attach("target", "mgmt", "ssh")
@@ -40,26 +39,21 @@ with infamy.Test() as test:
         running = target.get_config_dict("/ietf-system:system")
         users = running["system"]["authentication"]["user"]
 
-    with test.step(f"Verify regular user {USER} exists ..."):
+    with test.step(f"Verify regular user jacky exists"):
         jacky = next((user for user in users if user['name'] == USER), None)
         if not any(user['name'] == USER for user in users):
             test.fail()
 
-    with test.step(f"Verify user {USER} is not in wheel group ..."):
+    with test.step(f"Verify user jacky is not in wheel group"):
         wheel = tgtssh.runsh("grep wheel /etc/group").stdout
         if USER in wheel:
             test.fail()
 
-    with test.step(f"Verify user {USER} shell is not Bash ..."):
-        user = tgtssh.runsh(f"grep {USER} /etc/passwd").stdout
-        if "bash" in user:
-            test.fail()
-
-    with test.step(f"Verify user {USER} password is set correctly ..."):
+    with test.step(f"Verify user jacky password is set correctly"):
         if not tgtssh.runsh(f"sudo grep ':{PASS}:' /etc/shadow"):
             test.fail()
 
-    with test.step(f"Add {USER} user to admin group"):
+    with test.step(f"Add user jacky to admin group"):
         # Don't presume 'admin' user exists, only group
         nacm = target.get_config_dict("/ietf-netconf-acm:nacm")
         for group in nacm["nacm"]["groups"]["group"]:
@@ -68,16 +62,16 @@ with infamy.Test() as test:
                     group["user-name"].append(USER)
         target.put_config_dict("ietf-netconf-acm", nacm)
 
-    with test.step(f"Verify user {USER} is now in wheel group ..."):
+    with test.step(f"Verify user jacky is now in wheel group"):
         if not tgtssh.runsh(f"grep wheel /etc/group | grep '{USER}'"):
             test.fail()
 
-    with test.step(f"Verify user {USER} shell now is Bash ..."):
+    with test.step(f"Verify user jacky shell now is Bash"):
         user = tgtssh.runsh(f"grep {USER} /etc/passwd").stdout
         if "bash" not in user:
             test.fail()
 
-    with test.step(f"Change user {USER} to $factory$ password ..."):
+    with test.step(f"Change user jacky to $factory$ password ..."):
         running = target.get_config_dict("/ietf-system:system")
         users = running["system"]["authentication"]["user"]
 
@@ -87,7 +81,7 @@ with infamy.Test() as test:
                 break
         target.put_config_dict("ietf-system", running)
 
-    with test.step(f"Verify user {USER} exists and has new password ..."):
+    with test.step(f"Verify user jacky exists and has new password"):
         operational = target.get_data("/ietf-system:system/authentication")
         users = operational["system"]["authentication"]["user"]
 
@@ -104,13 +98,7 @@ with infamy.Test() as test:
         if found['password'] == PASS:
             test.fail()
 
-    with test.step(f"Verify user {USER} can log in with SSH ..."):
-        conn = ssh.Device("target", ssh.Location(address, factory, USER))
-        try:
-            pwd = conn.runsh(f"cat /etc/passwd | grep {USER}").stdout
-            print(f"Found {pwd.rstrip()}")
-        except Exception as err:
-            print(f"Failed connecting to target as {USER}: {err}")
-            test.fail()
-
+    with test.step(f"Verify user jacky can log in with SSH"):
+        testssh=ssh.Device("target", ssh.Location(address, USER, factory))
+        util.until(lambda: testssh.runsh("ls").returncode == 0)
     test.succeed()
