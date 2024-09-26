5461a443a3db88ab720a768cbbf77a40c7132d23
Mattias Walström <lazzer@gmail.com>
2024-09-26 14:24:24 +0200

    Upgrade rousette and required CPP-bindings
    First release of rousette.

--- package/rousette/rousette.mk
+++ package/rousette/rousette.mk
@@ -3,10 +3,8 @@
 # Rousette RESTconf server
 #
 ################################################################################
-
-ROUSETTE_VERSION = cf7a1d6eafc29ea708f18833ceb2f297dc2b7f74
-ROUSETTE_SITE = https://github.com/kernelkit/rousette.git
-ROUSETTE_SITE_METHOD = git
+ROUSETTE_VERSION = v1
+ROUSETTE_SITE = $(call github,CESNET,rousette,$(ROUSETTE_VERSION))
 ROUSETTE_LICENSE = Apache-2.0
 ROUSETTE_LICENSE_FILES = LICENSE
 ROUSETTE_DEPENDENCIES = boost docopt-cpp nghttp2-asio spdlog sysrepo-cpp linux-pam date-cpp
@@ -19,4 +17,3 @@ define ROUSETTE_USERS
 	yangnobody 333666 yangnobody 333666 * - - - Unauthenticated operations via RESTCONF
 endef
 $(eval $(cmake-package))
-
