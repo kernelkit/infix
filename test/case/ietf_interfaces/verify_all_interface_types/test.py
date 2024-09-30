64c51c7c944fa8637f0c3aa6a2444803a5287fcf
Mattias Walström <lazzer@gmail.com>
2024-09-30 20:50:03 +0200

    test: verify_all_interface_types: Update to generate correct test specification

--- test/case/ietf_interfaces/verify_all_interface_types/test.py
+++ test/case/ietf_interfaces/verify_all_interface_types/test.py
@@ -1,24 +1,19 @@
 #!/usr/bin/env python3
+#
+# lo     br-0                    br-Q.40           br-D         br-X
+#  |       |                       |                |            |
+#  o       o      eth-Q.10       br-Q           veth0a.20     eth-X.30
+#                         \     /    \              |            |
+#                          eth-Q     veth0b       veth0a        eth-X
+#                                         `---------'
+
 """
-```
- lo     br-0                    br-Q.40           br-D         br-X
-  |       |                       |                |            |
-  o       o      eth-Q.10       br-Q           veth0a.20     eth-X.30
-                         \     /    \              |            |
-                          eth-Q     veth0b       veth0a        eth-X
-                                         `---------'
-```
-Verify that all interface types can be created:
-1. Ethernet/Etherlike (ethX)
-2. Loopback (lo)
-3. Empty bridge (br-0)
-4. Ethernet/Etherlike (ethQ) as a bridge port in br-Q
-5. VETH pair: veth0a <--> veth0b, veth0b as a bridge port in br-Q
-6. VLAN:
-  1. ethQ.10 (VLAN 10) on top of an Ethernet/Etherlike interface (ethQ) 
-  2. br-Q.40 (VLAN 40) on top of a bridge (br-Q)
-  3. veth0a.20 (VLAN 20) on top of a VETH interface (veth0a) as a bridge port in br-D 
-  4. ethX.30 (VLAN 30) as a bridge port in br-X 
+Verify that all interface types can be created
+
+This test verify that all interface types can be created
+and also, tesing setting the configuration in sequal (this
+takes a little longer time than send it once)
+
 """
 
 import infamy
@@ -32,9 +27,9 @@ def verify_interface(target, interface, expected_type):
     actual_type = iface._iface_get_param(target, interface, "type")
 
     if expected_type == "infix-if-type:etherlike"  and actual_type == "infix-if-type:ethernet":
-        return  # Allow 'etherlike' to match 'ethernet' 
-    
-    assert actual_type == expected_type, f"Assertion failed! expected tpye: {expected_type}, actual type {actual_type}"   
+        return  # Allow 'etherlike' to match 'ethernet'
+
+    assert actual_type == expected_type, f"Assertion failed! expected tpye: {expected_type}, actual type {actual_type}"
 
 
 with infamy.Test() as test:
@@ -193,25 +188,28 @@ with infamy.Test() as test:
             ]
         }
     })
-    
-    interfaces_to_verify = {
-        loopback: "loopback",
-        eth_X: "etherlike",
-        eth_Q: "etherlike",
-        br_0: "bridge",
-        br_Q: "bridge",
-        br_X: "bridge",
-        br_D: "bridge",
-        veth_b: "veth",
-        veth_a: "veth",
-        veth_a_20: "vlan",
-        eth_Q_10: "vlan",
-        eth_X_30: "vlan",
-        br_Q_40: "vlan"
-    }
 
-    for interface, iface_type in interfaces_to_verify.items():
-        with test.step(f"Verify {iface_type} interface {interface}"):
-            verify_interface(target, interface, iface_type)
+    with test.step("Verify interface 'lo' is of type loopback"):
+        verify_interface(target, "lo", "loopback")
+
+    with test.step("Verify interfaces 'ethX' and 'ethQ' is of type 'ethernet' (or etherlike if running Qemu)"):
+         verify_interface(target, eth_X, "etherlike")
+         verify_interface(target, eth_Q, "etherlike")
+
+    with test.step("Verify interfaces 'br-0', 'br-X', 'br-D' and 'br-Q' is of type 'bridge'"):
+        verify_interface(target, "br-0", "bridge")
+        verify_interface(target, "br-X", "bridge")
+        verify_interface(target, "br-Q", "bridge")
+        verify_interface(target, "br-D", "bridge")
+
+    with test.step("Verify interfaces 'veth0a' and 'veth0b' is of type 'veth'"):
+        verify_interface(target, "veth0a", "veth")
+        verify_interface(target, "veth0b", "veth")
+
+    with test.step("Verify interfaces 'veth0a.20', 'ethQ.10', 'ethX.30', 'ethQ.10' and 'br-Q.40' is of type 'vlan'"):
+        verify_interface(target, "veth0a.20", "vlan")
+        verify_interface(target, f"{eth_X}.30", "vlan")
+        verify_interface(target, f"{eth_Q}.10", "vlan")
+        verify_interface(target, "br-Q.40", "vlan")
 
     test.succeed()
