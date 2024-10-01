240a2927254ef337655d533b536817122b3bfff9
Richard Alpe <richard@bit42.se>
2024-10-01 13:58:10 +0200

    test: remove explicit iface list from sysrepo emulator
    Yanger now has the ability to loop all interfaces (based on ip link),
    this means we can run once for all interfaces.
    
    Signed-off-by: Richard Alpe <richard@bit42.se>

--- test/case/cli/sysrepo-emulator.sh
+++ test/case/cli/sysrepo-emulator.sh
@@ -5,13 +5,10 @@ ROOT_PATH="$SCRIPT_PATH/../../../"
 
 YANGER_TOOL="$ROOT_PATH/src/statd/python/yanger/yanger.py"
 
-INTERFACES="br0 br1 e0 e1 e2 e3 e4"
-
-INTERFACE_OUTPUT_FILE="$(mktemp)"
 INTERFACES_OUTPUT_FILE="$(mktemp)"
 ROUTES_OUTPUT_FILE="$(mktemp)"
 cleanup() {
-    rm -f "$INTERFACE_OUTPUT_FILE"
+    rm -f "$INTERFACES_OUTPUT_FILE"
     rm -f "$ROUTES_OUTPUT_FILE"
 }
 trap cleanup EXIT
@@ -21,22 +18,10 @@ if [ ! -e "$YANGER_TOOL" ]; then
     exit 1
 fi
 
-
-for iface in $INTERFACES; do
-    if ! "$YANGER_TOOL" "ietf-interfaces" \
-          -t "$SCRIPT_PATH/system-output/" \
-          -p "$iface" >> "$INTERFACE_OUTPUT_FILE"; then
-        echo "Error, running yanger for interface $iface" >&2
-        exit 1
-    fi
-done
-
-if ! jq -s 'reduce .[] as $item
-       ({}; .["ietf-interfaces:interfaces"].interface +=
-        $item["ietf-interfaces:interfaces"].interface)' \
-      "$INTERFACE_OUTPUT_FILE" >> $INTERFACES_OUTPUT_FILE; then
-    echo "Error, merging yanger output data" >&2
-    exit 1
+if ! "$YANGER_TOOL" "ietf-interfaces" \
+	-t "$SCRIPT_PATH/system-output/" >> "$INTERFACES_OUTPUT_FILE"; then
+	echo "Error, running yanger for interface $iface" >&2
+	exit 1
 fi
 
 $YANGER_TOOL "ietf-routing" -t "$SCRIPT_PATH/system-output/" > "$ROUTES_OUTPUT_FILE"
