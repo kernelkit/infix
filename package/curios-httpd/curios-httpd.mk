6743223b29bbe0de26b360ded58b43736fdf52d7
Joachim Wiberg <troglobit@gmail.com>
2024-10-07 12:30:48 +0200

    package/curios-httpd: install unmodified OCI tarball in image
    Before:
    
        $ ls -lah output/target/lib/oci
        drwxr-xr-x  2 jacky users 4,0K Oct  7 12:26 .
        drwxr-xr-x 27 jacky users  20K Aug 29 06:45 ..
        -rw-r--r--  1 jacky users 1,8M Oct  7 12:19 curios-httpd-v24.05.0.tar.gz
    
    After:
    
        $ ls -lah output/target/lib/oci
        drwxr-xr-x  2 jacky users 4,0K Oct  7 12:26 .
        drwxr-xr-x 27 jacky users  20K Aug 29 06:45 ..
        -rw-r--r--  1 jacky users 281K Oct  7 12:28 curios-httpd-v24.05.0.tar.gz
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- package/curios-httpd/curios-httpd.mk
+++ package/curios-httpd/curios-httpd.mk
@@ -9,11 +9,11 @@ CURIOS_HTTPD_SOURCE = curios-httpd-oci-$(GO_GOARCH)-$(CURIOS_HTTPD_VERSION).tar.
 CURIOS_HTTPD_SITE = https://github.com/kernelkit/curiOS/releases/download/$(CURIOS_HTTPD_VERSION)
 CURIOS_HTTPD_LICENSE = GPL
 CURIOS_HTTPD_LICENSE_FILES = COPYING
-CURIOS_HTTPD_INSTALL_TARGET = YES
 
 define CURIOS_HTTPD_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/lib/oci
-	(cd $(@D)/.. && $(TAR) cfz $(TARGET_DIR)/lib/oci/$(notdir $(@D)).tar.gz $(notdir $(@D)))
+	cp $(CURIOS_HTTPD_DL_DIR)/$(CURIOS_HTTPD_SOURCE) \
+		$(TARGET_DIR)/lib/oci/$(notdir $(@D)).tar.gz
 endef
 
 $(eval $(generic-package))
