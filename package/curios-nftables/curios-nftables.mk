5cbe92f9ec8b503bd1ea1cd08515d9a7ff243870
Joachim Wiberg <troglobit@gmail.com>
2024-10-07 14:03:05 +0200

    package/curios-nftables: new package
    Allow bundling dedicated nftables container in Infix images.  With this
    container, the /etc/nftables.conf can be kept as part of the device's
    startup-config (base-64 coded binary format).  Highly useful in advanced
    setups, either with dedicated interfaces in the container, or when run
    in host network mode.
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

---
+++ package/curios-nftables/curios-nftables.mk
@@ -0,0 +1,20 @@
+################################################################################
+#
+# curios-nftables
+#
+################################################################################
+
+CURIOS_NFTABLES_VERSION = v24.05.0
+CURIOS_NFTABLES_SOURCE = curios-nftables-oci-$(GO_GOARCH)-$(CURIOS_NFTABLES_VERSION).tar.gz
+CURIOS_NFTABLES_SITE = https://github.com/kernelkit/curiOS/releases/download/$(CURIOS_NFTABLES_VERSION)
+CURIOS_NFTABLES_LICENSE = GPL
+CURIOS_NFTABLES_LICENSE_FILES = COPYING
+CURIOS_NFTABLES_INSTALL_TARGET = YES
+
+define CURIOS_NFTABLES_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/lib/oci
+	cp $(CURIOS_NFTABLES_DL_DIR)/$(CURIOS_NFTABLES_SOURCE) \
+		$(TARGET_DIR)/lib/oci/$(notdir $(@D)).tar.gz
+endef
+
+$(eval $(generic-package))
