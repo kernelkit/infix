f06a42987a4cfd2b040e569305a9c459fca8725c
Mattias Walström <lazzer@gmail.com>
2024-12-11 15:52:43 +0100

    srx: helpers: Remove duplicate functions that has been moved to libite
    Instead use the libite functions.

--- src/libsrx/src/helpers.h
+++ src/libsrx/src/helpers.h
@@ -7,25 +7,6 @@
 
 int vasprintf(char **strp, const char *fmt, va_list ap);
 
-int runbg(char *const args[], int delay);
-int run_status(int pid);
-
-int fexistf(const char *fmt, ...)
-	__attribute__ ((format (printf, 1, 2)));
-FILE *popenf(const char *type, const char *cmdf, ...)
-	__attribute__ ((format (printf, 2, 3)));
-
-int vreadllf(long long *value, const char *fmt, va_list ap);
-int readllf(long long *value, const char *fmt, ...)
-	__attribute__ ((format (printf, 2, 3)));
-int readdf(int *value, const char *fmt, ...)
-	__attribute__ ((format (printf, 2, 3)));
-
-int writedf(int value, const char *mode, const char *fmt, ...)
-	__attribute__ ((format (printf, 3, 4)));
-int writesf(const char *str, const char *mode, const char *fmt, ...)
-	__attribute__ ((format (printf, 3, 4)));
-
 char *unquote(char *buf);
 char *fgetkey(const char *file, const char *key);
 
