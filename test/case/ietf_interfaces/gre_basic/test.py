12b61465511221cce6876aa41a04698efe24182e
Mattias Walström <lazzer@gmail.com>
2025-01-10 14:04:35 +0100

    Add new generated specification files

--- test/case/ietf_interfaces/gre_basic/test.py
+++ test/case/ietf_interfaces/gre_basic/test.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python3
 """
-Basic IP GRE test
+Basic GRE connectivity test
 
 Test setting up IP GRE tunnels using IPv4 and IPv6,
-and then a connectivity test.
+and ends with a connectivity test.
 """
 
 import infamy
 
+class ArgumentParser(infamy.ArgumentParser):
+    def __init__(self):
+        super().__init__()
+        self.add_argument("--type")
+
 with infamy.Test() as test:
     with test.step("Set up topology and attach to target DUTs"):
-           env = infamy.Env()
-           left = env.attach("left", "mgmt")
-           right = env.attach("right", "mgmt")
-           _, leftlink = env.ltop.xlate("left", "link")
-           _, leftdata = env.ltop.xlate("left", "data")
-           _, rightlink = env.ltop.xlate("right", "link")
+        args=ArgumentParser()
+        env = infamy.Env(args=args)
+        type = env.args.type
+        left = env.attach("left", "mgmt")
+        right = env.attach("right", "mgmt")
 
 
     with test.step("Configure DUTs"):
@@ -23,7 +27,7 @@ with infamy.Test() as test:
             "interfaces": {
                 "interface": [
                 {
-                    "name": leftlink,
+                    "name": left["link"],
                     "ipv4": {
                         "address": [{
                             "ip": "192.168.50.1",
@@ -40,7 +44,7 @@ with infamy.Test() as test:
                     }
                 },
                 {
-                    "name": leftdata,
+                    "name": left["data"],
                     "ipv4": {
                         "address": [{
                             "ip": "192.168.10.1",
@@ -58,7 +62,7 @@ with infamy.Test() as test:
                 },
                 {
                     "name": "gre0",
-                    "type": "infix-if-type:gre",
+                    "type": f"infix-if-type:{type}",
                     "ipv4": {
                         "address": [{
                             "ip": "192.168.30.1",
@@ -74,7 +78,7 @@ with infamy.Test() as test:
                 },
                 {
                     "name": "gre6",
-                    "type": "infix-if-type:gre",
+                    "type": f"infix-if-type:{type}",
                     "ipv6": {
                         "address": [{
                             "ip": "2001:db8:3c4d:30::1",
@@ -95,7 +99,7 @@ with infamy.Test() as test:
                 "interfaces": {
                     "interface": [
                     {
-                        "name": rightlink,
+                        "name": right["link"],
                         "ipv4": {
                             "address": [{
                                 "ip": "192.168.50.2",
@@ -112,7 +116,7 @@ with infamy.Test() as test:
                     },
                     {
                         "name": "gre1",
-                        "type": "infix-if-type:gre",
+                        "type": f"infix-if-type:{type}",
                         "ipv4": {
                             "address": [{
                                 "ip": "192.168.30.2",
@@ -127,7 +131,7 @@ with infamy.Test() as test:
                     },
                     {
                         "name": "gre6",
-                        "type": "infix-if-type:gre",
+                        "type": f"infix-if-type:{type}",
                         "ipv6": {
                             "address": [{
                                 "ip": "2001:db8:3c4d:30::2",
@@ -171,16 +175,15 @@ with infamy.Test() as test:
             }
         })
     _, hport = env.ltop.xlate("host", "data")
-    with test.step("Test connectivity host:data to gre 10.0.0.2"):
+    with test.step(f"Verify connectivity host:data to 10.0.0.2"):
         with infamy.IsolatedMacVlan(hport) as ns0:
             ns0.addip("192.168.10.2")
             ns0.addroute("192.168.30.0/24", "192.168.10.1")
             ns0.must_reach("192.168.30.2")
-    with test.step("Test connectivity host:data to gre on right 2001:db8::c0a8:0a02"):
+    with test.step("Verify connectivity host:data to 2001:db8::c0a8:0a02"):
         with infamy.IsolatedMacVlan(hport) as ns0:
             ns0.addip("2001:db8:3c4d:10::2", prefix_length=64, proto="ipv6")
             ns0.addroute("2001:db8:3c4d:30::/64", "2001:db8:3c4d:10::1", proto="ipv6")
-            #breakpoint()
             ns0.must_reach("2001:db8:3c4d:30::2")
 
     test.succeed()
