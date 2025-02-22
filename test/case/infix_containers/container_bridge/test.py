5660f6239d9251a154fbd9a7870ba8d9f3fe4ebf
Joachim Wiberg <troglobit@gmail.com>
2025-02-22 11:51:08 +0100

    test: refine container bridge test
    The phys container test verifies custom mounts, so let's refine and
    consolidate this test to focus on docker bridge features.
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- test/case/infix_containers/container_bridge/test.py
+++ test/case/infix_containers/container_bridge/test.py
@@ -22,7 +22,7 @@ with infamy.Test() as test:
     NAME  = "web-docker0"
     DUTIP = "10.0.0.2"
     OURIP = "10.0.0.1"
-    BODY  = "<html><body><p>Kilroy was here</p></body></html>"
+    MESG  = "It works"
     URL   = f"http://{DUTIP}:8080/index.html"
 
     with test.step("Set up topology and attach to target DUT"):
@@ -32,61 +32,55 @@ with infamy.Test() as test:
         if not target.has_model("infix-containers"):
             test.skip()
 
-    with test.step("Create container 'web-docker0' from bundled OCI image"):
+    with test.step("Create httpd container from bundled OCI image"):
         _, ifname = env.ltop.xlate("target", "data")
-        data = to_binary(BODY)
-        target.put_config_dict("ietf-interfaces", {
-            "interfaces": {
-                "interface": [
-                    {
-                        "name": f"{ifname}",
-                        "ipv4": {
-                            "address": [{
-                                "ip": f"{DUTIP}",
-                                "prefix-length": 24
-                            }]
+        target.put_config_dicts({
+            "ietf-interfaces": {
+                "interfaces": {
+                    "interface": [
+                        {
+                            "name": f"{ifname}",
+                            "ipv4": {
+                                "address": [{
+                                    "ip": f"{DUTIP}",
+                                    "prefix-length": 24
+                                }]
+                            }
+                        }, {
+                            "name": "docker0",
+                            "type": "infix-if-type:bridge",
+                            "container-network": {
+                                "type": "bridge",
+                                "subnet": [
+                                    {"subnet": "172.17.0.0/16"},
+                                    {"subnet": "2a02:2789:724:eb8:1::/80"}
+                                ]
+                            }
                         }
-                    },
-                    {
-                        "name": "docker0",
-                        "type": "infix-if-type:bridge",
-                        "container-network": {
-                            "type": "bridge",
-                            "subnet": [
-                                { "subnet": "172.17.0.0/16" },
-                                { "subnet": "2a02:2789:724:eb8:1::/80" }
-                            ]
-                        }
-                    }
-                ]
-            }
-        })
-        target.put_config_dict("infix-containers", {
-            "containers": {
-                "container": [
-                    {
+                    ]
+                }
+            },
+            "infix-containers": {
+                "containers": {
+                    "container": [{
                         "name": f"{NAME}",
                         "image": f"oci-archive:{infamy.Container.HTTPD_IMAGE}",
-                        "command": "/usr/sbin/httpd -f -v -p 91",
-                        "mount": [
-                            {
-                                "name": "index.html",
-                                "content": f"{data}",
-                                "target": "/var/www/index.html"
-                            }
-                        ],
+                        "command": "/usr/sbin/httpd -f -v",
                         "network": {
-                            "interface": [
-                                { "name": "docker0" }
-                            ],
-                            "publish": [ "8080:91" ]
+                            "interface": [{
+                                "name": "docker0",
+                                "option": [
+                                    "interface_name=wan",
+                                    "ip=172.17.0.2"
+                                ],
+                            }],
+                            "publish": ["8080:80"]
                         }
-                    }
-                ]
-            }
-        })
+                    }]
+                }
+            }})
 
-    with test.step("Verify container 'web-docker0' has started"):
+    with test.step("Verify container has started"):
         c = infamy.Container(target)
         until(lambda: c.running(NAME), attempts=10)
 
@@ -95,9 +89,11 @@ with infamy.Test() as test:
 
     with infamy.IsolatedMacVlan(hport) as ns:
         ns.addip(OURIP)
-        with test.step("Verify basic DUT connectivity, host:data can ping DUT 10.0.0.2"):
+
+        with test.step("Verify DUT connectivity, host can reach 10.0.0.2"):
             ns.must_reach(DUTIP)
-        with test.step("Verify container 'web-docker0' is reachable on http://10.0.0.2:8080"):
-            until(lambda: url.nscheck(ns, "Kilroy was here"), attempts=10)
+
+        with test.step("Verify container is reachable on http://10.0.0.2:8080"):
+            until(lambda: url.nscheck(ns, MESG), attempts=10)
 
     test.succeed()
