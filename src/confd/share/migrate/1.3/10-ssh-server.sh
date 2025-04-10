454207c1857b9cf0518de31b738de40cf46378ba
Mattias Walström <lazzer@gmail.com>
2025-04-10 08:24:21 +0200

    Fix migration when SSH server settings exist
    This fix #1015

--- src/confd/share/migrate/1.3/10-ssh-server.sh
+++ src/confd/share/migrate/1.3/10-ssh-server.sh
@@ -3,14 +3,17 @@
 
 file=$1
 temp=${file}.tmp
-
-jq '.["infix-services:ssh"] = {
-      "enabled": true,
-      "hostkey": ["genkey"],
-      "listen": [
-        {"name": "ipv4", "address": "0.0.0.0", "port": 22},
-        {"name": "ipv6", "address": "::", "port": 22}
-      ]
-    }' "$file" > "$temp"
-
-mv "$temp" "$file"
+jq '
+if has("infix-services:ssh") then
+  .
+else
+  .["infix-services:ssh"] = {
+    "enabled": true,
+    "hostkey": ["genkey"],
+    "listen": [
+      {"name": "ipv4", "address": "0.0.0.0", "port": 22},
+      {"name": "ipv6", "address": "::", "port": 22}
+    ]
+  }
+end' $file > temp.json
+mv temp.json $file
