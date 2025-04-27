b7f110dbdcb1728257687b06ba2363d760143b12
Joachim Wiberg <troglobit@gmail.com>
2025-04-28 00:50:30 +0200

    package/finit: bump to v4.12
     - Improve notify:s6 readiness compatibility
     - Update `runparts` usage text
     - Fix variable overloading in urandom plugin
     - Fix buffer overwrite in urandom plugin, reported by Aaron Andersen
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- package/finit/finit.mk
+++ package/finit/finit.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FINIT_VERSION = 4.11
+FINIT_VERSION = 4.12
 FINIT_SITE = https://github.com/troglobit/finit/releases/download/$(FINIT_VERSION)
 FINIT_LICENSE = MIT
 FINIT_LICENSE_FILES = LICENSE
