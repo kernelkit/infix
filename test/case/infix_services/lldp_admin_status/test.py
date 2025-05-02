2cbba5cf1f0311ce15bb645229d49a578537b6da
Ahmed Karic <ahmkar@addiva.se>
2025-05-02 17:06:23 +0200

    test: Cleanup topologies

--- test/case/infix_services/lldp_admin_status/test.py
+++ test/case/infix_services/lldp_admin_status/test.py
@@ -22,7 +22,7 @@ def capture_traffic(iface, sec):
         return sniffer.output()
         
 def send_lldp_packet(iface, chassis_id, chassis_id_subtype, ttl=3):
-    eth = Ether(dst="01:80:c2:00:00:0e", type=0x88cc)
+    eth = Ether(src="02:01:02:03:04:05", dst="01:80:C2:00:00:0E", type=0x88cc)
     lldpdu = eth / LLDPDU() 
     lldpdu /= LLDPDUChassisID(subtype=chassis_id_subtype, id=chassis_id)
     lldpdu /= LLDPDUPortID(subtype=5, id=iface) 
