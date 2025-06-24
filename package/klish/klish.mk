fe031f1a1092558a56493f3c76d85ed531b75f80
Joachim Wiberg <troglobit@gmail.com>
2025-06-24 10:51:34 +0200

    package/klish: fix by-word movement and kill buffer support
     - Fix by-word movement, detect word barrier using non-alphanum chars
     - Fix delete word left/right, make sure to save word in kill buffer
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- package/klish/klish.mk
+++ package/klish/klish.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KLISH_VERSION = 019ebd2704e322b5d500f5687d526431e535eec8
+KLISH_VERSION = 1c5ae4bbb49a0d7e1425ca1afe8518b873ee84cb
 KLISH_SITE = https://github.com/kernelkit/klish.git
 #KLISH_VERSION = tags/3.0.0
 #KLISH_SITE = https://src.libcode.org/pkun/klish.git
