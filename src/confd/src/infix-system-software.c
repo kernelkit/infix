cc0edaf59c14e9bed6ebc1a0fe79b4cc4686153a
Mattias Walström <lazzer@gmail.com>
2025-07-11 16:27:16 +0200

    confd: Add support to upload images directly
    This add support for upgrading images with curl only, without FTP or
    similiar. The image needs to be base64 encoded.
    
    Example:
    
    TEMP_JSON=/tmp/restconf.txt
    echo -n '{"infix-system:input":{"image":"' > "$TEMP_JSON"
    base64 -w 0 "$BUNDLE_FILE" >> "$TEMP_JSON"
    echo '"}}' >> "$TEMP_JSON"
    
    curl -k \
      -X POST \
      -u "admin:admin" \
      -H "Content-Type: application/yang-data+json" \
      -H "Accept: application/yang-data+json" \
      -d @"$TEMP_JSON" \
      "https://192.168.30.1/restconf/operations/infix-system:install-bundle"

--- src/confd/src/infix-system-software.c
+++ src/confd/src/infix-system-software.c
@@ -1,6 +1,9 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 #include <srx/common.h>
 #include <srx/lyx.h>
+#include <openssl/bio.h>
+#include <openssl/evp.h>
+#include <sys/statvfs.h>
 
 #include "core.h"
 #include "rauc-installer.h"
@@ -21,24 +24,111 @@ static RaucInstaller *infix_system_sw_new_rauc(void)
 	return rauc;
 }
 
+static int base64_decode_inplace(char *input, size_t input_len, size_t *output_len)
+{
+	BIO *bio, *b64;
+	int decode_len;
+
+	bio = BIO_new_mem_buf(input, input_len);
+	if (!bio) {
+		return -1;
+	}
+
+	b64 = BIO_new(BIO_f_base64());
+	if (!b64) {
+		BIO_free(bio);
+		return -1;
+	}
+
+	BIO_set_flags(b64, BIO_FLAGS_BASE64_NO_NL);
+	bio = BIO_push(b64, bio);
+
+	/* Decode directly into the same buffer */
+	decode_len = BIO_read(bio, input, input_len);
+	BIO_free_all(bio);
+
+	if (decode_len < 0) {
+		return -1;
+	}
+
+	*output_len = decode_len;
+	return 0;
+}
+
 static int infix_system_sw_install(sr_session_ctx_t *session, uint32_t sub_id,
 				   const char *path, const sr_val_t *input,
 				   const size_t input_cnt, sr_event_t event,
 				   unsigned request_id, sr_val_t **output,
 				   size_t *output_cnt, void *priv)
 {
-	char *url = input->data.string_val;
 	sr_error_t srerr = SR_ERR_OK;
 	GError *raucerr = NULL;
-	RaucInstaller *rauc;
+	RaucInstaller *rauc = NULL;
+	char *install_source = NULL;
 
-	DEBUG("url:%s", url);
+	const char *url = NULL;
+	char *binary_data = NULL;
+	size_t binary_len = 0;
+	size_t decoded_len = 0;
+
+	for (size_t i = 0; i < input_cnt; i++) {
+		if (strcmp(input[i].xpath, "/infix-system:install-bundle/url") == 0) {
+			if (input[i].type == SR_STRING_T) {
+				url = input[i].data.string_val;
+			}
+		} else if (strcmp(input[i].xpath, "/infix-system:install-bundle/image") == 0) {
+			if (input[i].type == SR_BINARY_T) {
+				binary_data = (char *)input[i].data.binary_val;  // Cast away const for in-place decode
+				binary_len = strlen(binary_data);  // Length of base64 string
+			}
+		}
+	}
+
+	if (url) {
+		DEBUG("Installing from URL: %s", url);
+		install_source = (char *)url;
+
+	} else if (binary_data) {
+		const char *temp_dir = "/tmp";
+		char path[256];
+		FILE *fp;
+
+		DEBUG("Installing from uploaded binary data (%zu bytes base64)", binary_len);
+
+		if (base64_decode_inplace(binary_data, binary_len, &decoded_len) != 0) {
+			sr_session_set_netconf_error(session, "application", "invalid-value",
+						     NULL, NULL, "Failed to decode base64 image data", 0);
+			srerr = SR_ERR_INVAL_ARG;
+			goto cleanup;
+		}
+
+		fmkpath(0775, "%s/images", temp_dir);
+		snprintf(path, sizeof(path), "%s/images/install_bundle", temp_dir);
+
+		fp = fopen(path, "wb");
+		if (!fp) {
+			ERROR("Could not open %s", path);
+			return SR_ERR_NO_MEMORY;
+		}
+
+		fwrite(binary_data, sizeof(char), decoded_len, fp);
+		fclose(fp);
+		install_source = path;
+
+	} else {
+		ERROR("Unknown source");
+		return 0;
+	}
 
 	rauc = infix_system_sw_new_rauc();
-	if (!rauc)
-		return SR_ERR_INTERNAL;
+	if (!rauc) {
+		sr_session_set_netconf_error(session, "application", "operation-failed",
+					     NULL, NULL, "Failed to initialize RAUC installer", 0);
+		srerr = SR_ERR_INTERNAL;
+		goto cleanup;
+	}
 
-	rauc_installer_call_install_sync(rauc, url, NULL, &raucerr);
+	rauc_installer_call_install_sync(rauc, install_source, NULL, &raucerr);
 	if (raucerr) {
 		sr_session_set_netconf_error(session, "application", "operation-failed",
 					     NULL, NULL, raucerr->message, 0);
@@ -46,13 +136,18 @@ static int infix_system_sw_install(sr_session_ctx_t *session, uint32_t sub_id,
 		srerr = SR_ERR_OPERATION_FAILED;
 	}
 
-	g_object_unref(rauc);
+cleanup:
+	if (rauc) {
+		g_object_unref(rauc);
+	}
+
 	return srerr;
 }
 
+
 /*
   boot order can only be: primary, secondary and net, limited by model
- */
+*/
 static int infix_system_sw_set_boot_order(sr_session_ctx_t *session, uint32_t sub_id,
 					  const char *path, const sr_val_t *input,
 					  const size_t input_cnt, sr_event_t event,
