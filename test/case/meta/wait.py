8920297d810560fab66580231014757d1b3b6871
Mattias Walström <lazzer@gmail.com>
2025-08-25 20:40:15 +0200

    test: meta: wait Test both NETCONF and RESTCONF, always
    Without resetting args.transport the situation may occur when wait
    running NETCONF and the next test RESTCONF. If this happen, the other
    test may fail.

--- test/case/meta/wait.py
+++ test/case/meta/wait.py
@@ -2,37 +2,26 @@
 
 import time
 import infamy
-import infamy.neigh
-
+from infamy.neigh import ll6ping
+from infamy.util import until, is_reachable
 
 TIMEOUT = 300
 
-
-def ll6ping(node):
-    cport, nport = env.ptop.get_mgmt_link(ctrl, node)
-    neigh = infamy.neigh.ll6ping(cport, flags=["-w1", "-c1", "-L", "-n"])
-    if neigh:
-        print(f"Found {neigh} on {cport} (connected to {node}:{nport})")
-        return neigh
-
-    return None
-
-
-def is_reachable(node, env):
-    neigh = ll6ping(node)
-    if not neigh:
+def infixen_reachable(node, ctrl, env):
+    cport, _ = env.ptop.get_mgmt_link(ctrl, node)
+    ip = ll6ping(cport)
+    if not ip:
         return False
 
-    return infamy.util.is_reachable(neigh, env, env.ptop.get_password(node))
-
+    return is_reachable(ip, env, env.ptop.get_password(node))
 
 with infamy.Test() as test:
     with test.step("Initialize"):
         # The test is designed to be run on a physical topology.
         env = infamy.Env(ltop=False)
-
         ctrl = env.ptop.get_ctrl()
         infixen = env.ptop.get_infixen()
+        env.args.transport = None # To wait for RESTCONF and NETCONF
 
     with test.step(f"Reach {infixen}"):
         print(f"Waiting for {infixen} to come up, timeout: {TIMEOUT / 60} min")
@@ -40,7 +29,7 @@ with infamy.Test() as test:
 
         while infixen and time.time() < timeout:
             time.sleep(1)
-            infixen = [node for node in infixen if not is_reachable(node, env)]
+            infixen = [node for node in infixen if not infixen_reachable(node, ctrl, env)]
 
         if infixen:
             print(f"Unable to reach {infixen}")
