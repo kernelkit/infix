97f38a320c09d6531e99504f72a9952a1c25e6cb
Joachim Wiberg <troglobit@gmail.com>
2025-09-27 10:33:20 +0200

    confd: new optional arguments to install-bundle rpc
    Add the following two optional flags to the install-bundle rpc:
    
     - verify-certificate (default: True)
     - verify-compatible (default: True)
    
    Issue #1080
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- src/confd/src/infix-system-software.c
+++ src/confd/src/infix-system-software.c
@@ -27,20 +27,55 @@ static int infix_system_sw_install(sr_session_ctx_t *session, uint32_t sub_id,
 				   unsigned request_id, sr_val_t **output,
 				   size_t *output_cnt, void *priv)
 {
-	char *url = input->data.string_val;
+	char *url = NULL;
+	gboolean verify_certificate = TRUE;  /* default value */
+	gboolean verify_compatible = TRUE;   /* default value */
 	sr_error_t srerr = SR_ERR_OK;
 	GError *raucerr = NULL;
 	RaucInstaller *rauc;
+	GVariantBuilder builder;
 	GVariant *args;
 
-	DEBUG("url:%s", url);
+	/* Parse input parameters */
+	for (size_t i = 0; i < input_cnt; i++) {
+		const sr_val_t *val = &input[i];
+		const char *name;
+
+		name = strrchr(val->xpath, '/');
+		if (!name)
+			continue;
+		name++; /* skip '/' */
+
+		if (strcmp(name, "url") == 0) {
+			url = val->data.string_val;
+		} else if (strcmp(name, "verify-certificate") == 0) {
+			verify_certificate = val->data.bool_val;
+		} else if (strcmp(name, "verify-compatible") == 0) {
+			verify_compatible = val->data.bool_val;
+		}
+	}
+
+	if (!url) {
+		sr_session_set_netconf_error(session, "application", "missing-element",
+					     NULL, NULL, "url parameter is required", 0);
+		return SR_ERR_INVAL_ARG;
+	}
+
+	DEBUG("url:%s verify-certificate:%s verify-compatible:%s", url,
+	      verify_certificate ? "true" : "false",
+	      verify_compatible ? "true" : "false");
 
 	rauc = infix_system_sw_new_rauc();
 	if (!rauc)
 		return SR_ERR_INTERNAL;
 
-	/* Empty args dictionary for InstallBundle method, for now. */
-	args = g_variant_new("a{sv}", NULL);
+	/* Build args dictionary for InstallBundle method */
+	g_variant_builder_init(&builder, G_VARIANT_TYPE("a{sv}"));
+	g_variant_builder_add(&builder, "{sv}", "tls-no-verify",
+			      g_variant_new_boolean(!verify_certificate));
+	g_variant_builder_add(&builder, "{sv}", "ignore-compatible",
+			      g_variant_new_boolean(!verify_compatible));
+	args = g_variant_builder_end(&builder);
 
 	rauc_installer_call_install_bundle_sync(rauc, url, args, NULL, &raucerr);
 	if (raucerr) {
