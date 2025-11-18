5ff54d27bbb1e9e45c8094ad68fd8ed2769d8671
Tobias Waldekranz <tobias@waldekranz.com>
2025-11-18 20:40:57 +0100

    board/common: Remove broken "Traditional FIT image"
    We have not installed .dtb:s to $O/images/ for quite some time, and
    nobody cared. That goes to show that this is not really used.
    
    The image is still useful at times, so if it needed in the future,
    then we can resurrect it from the logs and refactor it to an image
    package.

--- board/aarch64/board.mk
+++ board/aarch64/board.mk
@@ -20,13 +20,6 @@ board-enable-qemu-uboot:
 			'$$(BR2_EXTERNAL_INFIX_PATH)/board/common/uboot/extras.config' \
 		--enable TARGET_UBOOT_FORMAT_DTB
 
-.PHONY: board-enable-sparx-fit
-board-enable-sparx-fit:
-	@$(call IXMSG,"Enabling SparX-5i compatible FIT options")
-	@BR2_PREFIX= ./utils/config --file $(BR2_CONFIG) \
-	 	--enable FIT_IMAGE \
-	 	--set-str FIT_KERNEL_LOAD_ADDR "0x7 0x00000000"
-
 .PHONY: board-sparx-flash-uboot
 board-sparx-flash-uboot: $(BINARIES_DIR)/u-boot.bin
 	@grep -q 'BR2_TARGET_UBOOT_BOARD_DEFCONFIG="mscc_fireant_pcb135_emmc"' $(BR2_CONFIG) || \
