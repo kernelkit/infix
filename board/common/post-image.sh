af7a4cf8b57e78ec80a535004539183265f48c06
Tobias Waldekranz <tobias@waldekranz.com>
2025-11-19 21:03:33 +0100

    board/common: Move README.md installation to separate make target

--- board/common/post-image.sh
+++ board/common/post-image.sh
@@ -1,7 +1,1 @@
 #!/bin/sh
-
-# Only for regular builds, not bootloader-only builds
-if [ "$BR2_TARGET_ROOTFS_SQUASHFS" = "y" ]; then
-    # Quick intro for beginners, with links to more information
-    cp "$BR2_EXTERNAL_INFIX_PATH/board/common/README.txt" "$BINARIES_DIR/"
-fi
