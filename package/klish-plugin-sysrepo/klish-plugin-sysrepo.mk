2e06b667f0fa2c12b7523595b66396dfd646d60e
Mattias Walström <lazzer@gmail.com>
2025-12-30 08:14:19 +0100

    klish: followup 8a638fa26 allow `when` expression to depend on defaults
    If a when expssion depended on a default value it always got evaulated
    to false.

--- package/klish-plugin-sysrepo/klish-plugin-sysrepo.mk
+++ package/klish-plugin-sysrepo/klish-plugin-sysrepo.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KLISH_PLUGIN_SYSREPO_VERSION = d3751b91cb858ece9be138f0175502e43e3fdad1
+KLISH_PLUGIN_SYSREPO_VERSION = a1156954447ac663409b143dc67da7c15cf649ff
 KLISH_PLUGIN_SYSREPO_SITE = https://github.com/kernelkit/klish-plugin-sysrepo.git
 #KLISH_PLUGIN_SYSREPO_VERSION = cdd3eb51a7f7ee0ed5bd925fa636061d3b1b85fb
 #KLISH_PLUGIN_SYSREPO_SITE = https://src.libcode.org/pkun/klish-plugin-sysrepo.git
