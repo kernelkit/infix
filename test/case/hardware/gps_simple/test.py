ec18ed7dd5cd82193261b68b700ce028db937bf2
Mattias Walström <lazzer@gmail.com>
2026-02-22 19:42:18 +0100

    test: gps_simple

--- test/case/hardware/gps_simple/test.py
+++ test/case/hardware/gps_simple/test.py
@@ -9,7 +9,7 @@ which appears as a virtio serial port inside the guest.
 """
 import infamy
 import infamy.gps as gps
-from infamy import until
+from infamy.util import until, wait_boot
 
 lat=27.9881
 lon=86.9250
@@ -18,6 +18,31 @@ alt=8848.86
 
 def _near(a, b, tol):
     return abs(a - b) <= tol
+
+def verify_position(target):
+    state = gps.get_gps_state(target)
+
+    try:
+        lat = float(state["latitude"])
+        lon = float(state["longitude"])
+        alt = float(state["altitude"])
+    except (KeyError, TypeError, ValueError):
+        test.fail()
+
+        if not _near(lat, lat, 1e-4):
+            test.fail()
+        if not _near(lon, lon, 1e-4):
+            test.fail()
+        if not _near(alt, alt, 0.2):
+            test.fail()
+
+        try:
+            sat_used = int(state["satellites-used"])
+        except (KeyError, TypeError, ValueError):
+            test.fail()
+
+        if sat_used != 8:
+            test.fail()
 
 with infamy.Test() as test:
     with test.step("Set up topology and attach to target DUT"):
@@ -54,28 +79,21 @@ with infamy.Test() as test:
             until(lambda: gps.has_fix(target), attempts=60)
 
         with test.step("Verify the position is near the coordinates you test with"):
-            state = gps.get_gps_state(target)
+            verify_position(target)
 
-            try:
-                lat = float(state["latitude"])
-                lon = float(state["longitude"])
-                alt = float(state["altitude"])
-            except (KeyError, TypeError, ValueError):
+        with test.step("Save the configuration to startup configuration and reboot"):
+            target.startup_override()
+            target.copy("running", "startup")
+            target.reboot()
+            if not wait_boot(target, env):
                 test.fail()
+            target = env.attach("target", "mgmt", test_reset=False)
 
-            if not _near(lat, lat, 1e-4):
-                test.fail()
-            if not _near(lon, lon, 1e-4):
-                test.fail()
-            if not _near(alt, alt, 0.2):
-                test.fail()
+        with test.step("Verify GPS is activated"):
+            until(lambda: gps.is_activated(target), attempts=500)
 
-            try:
-                sat_used = int(state["satellites-used"])
-            except (KeyError, TypeError, ValueError):
-                test.fail()
-
-            if sat_used != 8:
-                test.fail()
-
+        with test.step("Verify GPS has a fix"):
+            until(lambda: gps.has_fix(target), attempts=60)
+        with test.step("Verify the position is near the coordinates you test with"):
+            verify_position(target)
     test.succeed()
