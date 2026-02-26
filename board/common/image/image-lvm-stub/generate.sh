f81de1d3ecb8f24347d185bafc590734ef97b273
Tobias Waldekranz <tobias@waldekranz.com>
2026-02-26 14:25:30 +0000

    image-lvm-stub: Base image for use with 'make run'

---
+++ board/common/image/image-lvm-stub/generate.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+set -e
+
+
+mkdir -p "${WORKDIR}"/root
+rm -rf   "${WORKDIR}"/tmp
+mkdir -p "${WORKDIR}"/tmp
+
+"${BR2_EXTERNAL_INFIX_PATH}"/utils/lvm-mkinternal >"${WORKDIR}"/stub.lvm
+
+cat <<EOF >"${WORKDIR}"/genimage.cfg
+image lvm-stub.disk {
+	hdimage {
+		partition-table-type = "gpt"
+	}
+
+	partition internal {
+		growfs = "true"
+		image = "stub.lvm"
+		partition-type-uuid = "lvm"
+	}
+}
+
+# Silence genimage warnings
+config {}
+EOF
+
+genimage \
+    --tmppath    "${WORKDIR}"/tmp  \
+    --rootpath   "${WORKDIR}"/root \
+    --inputpath  "${WORKDIR}"      \
+    --outputpath "${BINARIES_DIR}" \
+    --config "${WORKDIR}"/genimage.cfg
