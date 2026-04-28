f9ea5567c4481de2a745505332773ff0fc2a0111
Joachim Wiberg <troglobit@gmail.com>
2026-04-28 09:28:14 +0200

    package/finit: bump to v4.17
    https://github.com/finit-project/finit/releases/tag/4.17
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- package/finit/finit.mk
+++ package/finit/finit.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FINIT_VERSION = 4.16
+FINIT_VERSION = 4.17
 FINIT_SITE = https://github.com/troglobit/finit/releases/download/$(FINIT_VERSION)
 FINIT_LICENSE = MIT
 FINIT_LICENSE_FILES = LICENSE
