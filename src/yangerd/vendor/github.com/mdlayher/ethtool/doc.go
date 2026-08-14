55fc0ab324b29f4b6c0fa3f5dfdda35c2d17edd4
Mattias Walström <lazzer@gmail.com>
2026-08-14 13:57:33 +0200

    yangerd: Remove mdlayher/ethtool, not used anymore

--- src/yangerd/vendor/github.com/mdlayher/ethtool/doc.go
+++
@@ -1,4 +0,0 @@
-// Package ethtool allows control of the Linux ethtool generic netlink
-// interface. For more information, see:
-// https://www.kernel.org/doc/html/latest/networking/ethtool-netlink.html.
-package ethtool
